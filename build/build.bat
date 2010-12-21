1e124425b27705b0013b0619825e817ec5d5fee7
Demis Bellot <demis.bellot@gmail.com>
2010-12-21 23:29:52 +0000

    change build scripts to also deploy into ServiceStack\lib ILMERGE foler

--- build/build.bat
+++ build/build.bat
@@ -13,10 +13,15 @@ SET PROJ_LIBS=%PROJ_LIBS% ..\src\ServiceStack.OrmLite\bin\%BUILD%\ServiceStack.O
 ILMerge.exe /ndebug /t:library /out:ServiceStack.OrmLite.dll %PROJ_LIBS%
 COPY *.dll %DEPLOY_PATH%
 
-COPY ..\src\ServiceStack.OrmLite\bin\%BUILD%\ServiceStack.OrmLite.dll C:\src\ServiceStack\release\lib
-COPY ..\src\ServiceStack.OrmLite\bin\%BUILD%\ServiceStack.OrmLite.pdb C:\src\ServiceStack\release\lib
-COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.dll C:\src\ServiceStack\release\lib
-COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.pdb C:\src\ServiceStack\release\lib
-COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.dll C:\src\ServiceStack\release\lib
-COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.pdb C:\src\ServiceStack\release\lib
+COPY ..\src\ServiceStack.OrmLite\bin\%BUILD%\ServiceStack.OrmLite.dll ..\..\ServiceStack\release\lib
+COPY ..\src\ServiceStack.OrmLite\bin\%BUILD%\ServiceStack.OrmLite.pdb ..\..\ServiceStack\release\lib
+COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.dll ..\..\ServiceStack\release\lib
+COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.pdb ..\..\ServiceStack\release\lib
+COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.dll ..\..\ServiceStack\release\lib
+COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.pdb ..\..\ServiceStack\release\lib
+
+COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.dll ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.pdb ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.dll ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.pdb ..\..\ServiceStack\lib
 
