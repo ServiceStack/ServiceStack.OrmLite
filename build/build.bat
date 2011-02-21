575832be983ff32685ce2be44b366e4fbf2dd714
mythz <demis.bellot@gmail.com>
2011-02-21 11:13:46 +0000

    change the configuration of ServiceStack.OrmLite.Sqlite to always be x86 (32bit) required since it references a 32bit unmanaged dll

--- build/build.bat
+++ build/build.bat
@@ -5,6 +5,7 @@ COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..
 COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\release\latest\ServiceStack.OrmLite
 
 COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib\tests
 
 COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.Examples\lib
 
