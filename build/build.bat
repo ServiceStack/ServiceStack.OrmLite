5f4545434ade15bc54ba153ed15e5a987fd24d4d
mythz <demis.bellot@gmail.com>
2011-02-23 14:30:23 +0000

    checked in 32bit SQlite .NET project which is symetric to the x64 project so that they are bin compatible

--- build/build.bat
+++ build/build.bat
@@ -3,7 +3,10 @@ SET BUILD=Release
 
 COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.* ..\..\ServiceStack\release\latest\ServiceStack.OrmLite
 
-COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\ServiceStack.OrmLite.Sqlite64.* ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x64
+COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\ServiceStack.OrmLite.SqliteNET.* ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x32
+COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\System.Data.SQLite.dll ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x32
+
+COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\ServiceStack.OrmLite.SqliteNET.* ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x64
 COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\System.Data.SQLite.dll ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x64
 
 COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
