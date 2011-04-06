b1651938510058ee04bf5a84863f0f4489082761
Demis Bellot <demis.bellot@gmail.com>
2011-04-06 01:38:33 +0100

    Added NuGet packages for Sqlite32 Sqlite64 and SqlServer OrmLite providers

---
+++ NuGet/NuGetPack.cmd
@@ -0,0 +1,3 @@
+nuget pack ServiceStack.OrmLite.Sqlite32\servicestack.ormlite.sqlite32.nuget
+nuget pack ServiceStack.OrmLite.Sqlite64\servicestack.ormlite.sqlite64.nuget
+nuget pack ServiceStack.OrmLite.SqlServer\servicestack.ormlite.sqlserver.nuget
