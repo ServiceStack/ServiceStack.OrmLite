b9365cde9641bcee6002b04f20349668cb552c61
Thomas Grassauer <thomas.grassauer@counity.at>
2012-01-29 22:31:28 +0100

    added mysql dialect

---
+++ src/ServiceStack.OrmLite.MySql/MySqlDialectProvider.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace ServiceStack.OrmLite.MySql
+{
+    public class MySqlDialectProvider : OrmLiteDialectProviderBase
+    {
+        public static MySqlDialectProvider Instance = new MySqlDialectProvider();
+
+        private MySqlDialectProvider()
+        {
+            base.AutoIncrementDefinition = "AUTO_INCREMENT";
+            base.IntColumnDefinition = "int(11)";
+            base.BoolColumnDefinition = "bit(1)";
+            base.TimeColumnDefinition = "time";
+            base.DecimalColumnDefinition = "decimal(38,6)";
+            base.GuidColumnDefinition = "char(32)";
+            base.DefaultStringLength = 255;
+            base.InitColumnTypeMap();
+        }
+
+        public override string EscapeParam(object paramValue)
+        {
+            return paramValue.ToString().Replace("'", @"\'");
+        }
+
+        public override IDbConnection CreateConnection(string connectionString, Dictionary<string, string> options)
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public override string GetQuotedValue(object value, Type fieldType)
+        {
+            if (value == null) return "NULL";
+
+            if (fieldType == typeof(DateTime))
+            {
+                var dateValue = (DateTime)value;
+                const string iso8601Format = "yyyy-MM-dd HH:mm:ss.fff";
+                return base.GetQuotedValue(dateValue.ToString(iso8601Format), typeof(string));
+            }
+            if (fieldType == typeof(Guid))
+            {
+                var guidValue = (Guid)value;
+                return base.GetQuotedValue(guidValue.ToString("N"), typeof(string));
+            }
+
+            return base.GetQuotedValue(value, fieldType);
+        }
+
+        public override object ConvertDbValue(object value, Type type)
+        {
+            if (value == null || value is DBNull) return null;
+
+            if (type == typeof(bool))
+            {
+                var intVal = int.Parse(value.ToString());
+                return intVal != 0;
+            }
+
+            return base.ConvertDbValue(value, type);
+        }
+
+        public override string GetTableNameDelimited(ModelDefinition modelDef)
+        {
+            return string.Format("`{0}`", modelDef.ModelName);
+        }
+
+        public override string GetNameDelimited(string columnName)
+        {
+            return string.Format("`{0}`", columnName);
+        }
+
+        public override long GetLastInsertId(IDbCommand command)
+        {
+            command.CommandText = "SELECT LAST_INSERT_ID()";
+            var result = command.ExecuteScalar();
+            if (result is DBNull) return default(long);
+            return Convert.ToInt64(result);
+        }
+    }
+}
