728a26587bcdb00c8bc4bcc43ff03baf5c9904ab
Demis Bellot <demis.bellot@gmail.com>
2012-02-01 01:43:26 -0500

    Update with MySQL and Firebird NuGet builds.

--- NuGet/NuGetPack.cmd
+++ NuGet/NuGetPack.cmd
@@ -1,3 +1,6 @@
 nuget pack ServiceStack.OrmLite.Sqlite32\servicestack.ormlite.sqlite32.nuspec
 nuget pack ServiceStack.OrmLite.Sqlite64\servicestack.ormlite.sqlite64.nuspec
 nuget pack ServiceStack.OrmLite.SqlServer\servicestack.ormlite.sqlserver.nuspec
+nuget pack ServiceStack.OrmLite.MySql\servicestack.ormlite.mysql.nuspec
+nuget pack ServiceStack.OrmLite.MySql\servicestack.ormlite.firebird.nuspec
+
--- build/build.bat
+++ build/build.bat
@@ -4,8 +4,12 @@ SET BUILD=Release
 COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib
 COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib
 COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.SqlServer\lib
+COPY ..\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.MySql\lib
+COPY ..\src\ServiceStack.OrmLite.Firebird\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Firebird\lib
 COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\System.Data.SQLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib
 COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\System.Data.SQLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib
+COPY ..\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\mysql.data.dll ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.MySql\lib
+COPY ..\src\ServiceStack.OrmLite.Firebird\bin\%BUILD%\FirebirdSql.Data.FirebirdClient.dll ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Firebird\lib
 
 COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\*.* ..\..\ServiceStack\release\latest\ServiceStack.OrmLite
 COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\ServiceStack.OrmLite.SqliteNET.*  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x32
@@ -19,3 +23,4 @@ COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.* ..
 
 COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.Examples\lib
 COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.Contrib\lib
+
