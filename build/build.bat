fbbe3dfb02e22fd90aaa9440b965d76591c2548f
Demis Bellot <demis.bellot@gmail.com>
2012-07-23 04:12:28 -0400

    Add .NET 4.0 version of 32/64bit unmanaged mixed-assemblies. You can't run .NET Mixed assemblies in .NET 4.0 and vice-versa

--- build/build.bat
+++ build/build.bat
@@ -1,21 +1,29 @@
 REM SET BUILD=Debug
 SET BUILD=Release
 
-COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib
-COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib
+COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib\net35
+COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib\net40
+COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib\net35
+COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\ServiceStack.OrmLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib\net40
 COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.SqlServer\lib
 COPY ..\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.MySql\lib
 COPY ..\src\ServiceStack.OrmLite.PostgreSQL\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.PostgreSQL\lib
 COPY ..\src\ServiceStack.OrmLite.Firebird\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Firebird\lib
-COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\System.Data.SQLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib
-COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\System.Data.SQLite.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib
 COPY ..\src\ServiceStack.OrmLite.Firebird\bin\%BUILD%\FirebirdSql.Data.FirebirdClient.dll ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Firebird\lib
+
+COPY ..\lib\x32\net35\System.Data.SQLite.dll  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib\net35
+COPY ..\lib\x32\net40\System.Data.SQLite.dll  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite32\lib\net40
+COPY ..\lib\x64\net35\System.Data.SQLite.dll  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib\net35
+COPY ..\lib\x64\net40\System.Data.SQLite.dll  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Sqlite64\lib\net40
+
 
 COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\*.* ..\..\ServiceStack\release\latest\ServiceStack.OrmLite
 COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\ServiceStack.OrmLite.SqliteNET.*  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x32
 COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\ServiceStack.OrmLite.SqliteNET.*  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x64
-COPY ..\src\ServiceStack.OrmLite.Sqlite32\bin\%BUILD%\System.Data.SQLite.*  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x32
-COPY ..\src\ServiceStack.OrmLite.Sqlite64\bin\%BUILD%\System.Data.SQLite.*  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x64
+COPY ..\lib\x32\net35\System.Data.SQLite.dll  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x32\net35
+COPY ..\lib\x32\net40\System.Data.SQLite.dll  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x32\net40
+COPY ..\lib\x64\net35\System.Data.SQLite.dll  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x64\net35
+COPY ..\lib\x64\net40\System.Data.SQLite.dll  ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\x64\net40
 
 COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
 COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
