59a94a5dd591a52fa0e5aca8a1f00c08a67c2d1f
Daniel Hobbs <dhobbs@checkbox.com>
2012-08-16 16:58:23 -0600

    Remove unnecessary using statement. oops

--- src/ServiceStack.OrmLite/OrmLiteWriteExtensions.cs
+++ src/ServiceStack.OrmLite/OrmLiteWriteExtensions.cs
@@ -15,7 +15,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using ServiceStack.Common.Utils;
 using ServiceStack.Logging;
 
