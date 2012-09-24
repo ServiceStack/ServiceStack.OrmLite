9a3e8a8aef265eef406c6368a8fe593756b0609a
Thomas Grassauer <thomas.grassauer@counity.at>
2012-09-24 07:55:11 +0200

    fixed bool bug in firebird expression visitor

--- src/ServiceStack.OrmLite.Firebird/FirebirdSqlExpressionVisitor.cs
+++ src/ServiceStack.OrmLite.Firebird/FirebirdSqlExpressionVisitor.cs
@@ -1,9 +1,130 @@
 using System;
+using System.Linq.Expressions;
 
 namespace ServiceStack.OrmLite.Firebird
 {
 	public class FirebirdSqlExpressionVisitor<T>:SqlExpressionVisitor<T>
 	{
+	    private readonly string _trueExpression;
+	    private readonly string _falseExpression;
+
+	    public FirebirdSqlExpressionVisitor()
+        {
+            _trueExpression = string.Format("({0}={1})", GetQuotedTrueValue(), GetQuotedTrueValue());
+            _falseExpression = string.Format("({0}={1})", GetQuotedTrueValue(), GetQuotedFalseValue());
+        } 
+
+	    protected override object VisitBinary(BinaryExpression b)
+        {
+            object left, right;
+            var operand = BindOperant(b.NodeType);   //sep= " " ??
+            if (operand == "AND" || operand == "OR")
+            {
+                MemberExpression m = b.Left as MemberExpression;
+                if (m != null && m.Expression != null)
+                {
+                    string r = VisitMemberAccess(m).ToString();
+                    left = string.Format("{0}={1}", r, GetQuotedTrueValue());
+                }
+                else
+                {
+                    left = Visit(b.Left);
+                }
+                m = b.Right as MemberExpression;
+                if (m != null && m.Expression != null)
+                {
+                    string r = VisitMemberAccess(m).ToString();
+                    right = string.Format("{0}={1}", r, GetQuotedTrueValue());
+                }
+                else
+                {
+                    right = Visit(b.Right);
+                }
+
+                if (left as PartialSqlString == null && right as PartialSqlString == null)
+                {
+                    var result = Expression.Lambda(b).Compile().DynamicInvoke();
+                    return new PartialSqlString(OrmLiteConfig.DialectProvider.GetQuotedValue(result, result.GetType()));
+                }
+
+                if (left as PartialSqlString == null)
+                    left = ((bool)left) ? GetTrueExpression() : GetFalseExpression();
+                if (right as PartialSqlString == null)
+                    right = ((bool)right) ? GetTrueExpression() : GetFalseExpression();
+            }
+            else
+            {
+                left = Visit(b.Left);
+                right = Visit(b.Right);
+
+                if (left as EnumMemberAccess != null)
+                {
+                    var enumType = ((EnumMemberAccess)left).EnumType;
+
+                    //enum value was returned by Visit(b.Right)
+                    long numvericVal;
+                    if (Int64.TryParse(right.ToString(), out numvericVal))
+                        right = OrmLiteConfig.DialectProvider.GetQuotedValue(Enum.ToObject(enumType, numvericVal).ToString(),
+                                                                     typeof(string));
+                    else
+                        right = OrmLiteConfig.DialectProvider.GetQuotedValue(right, right.GetType());
+                }
+                else if (right as EnumMemberAccess != null)
+                {
+                    var enumType = ((EnumMemberAccess)right).EnumType;
+
+                    //enum value was returned by Visit(b.Left)
+                    long numvericVal;
+                    if (Int64.TryParse(left.ToString(), out numvericVal))
+                        left = OrmLiteConfig.DialectProvider.GetQuotedValue(Enum.ToObject(enumType, numvericVal).ToString(),
+                                                                     typeof(string));
+                    else
+                        left = OrmLiteConfig.DialectProvider.GetQuotedValue(left, left.GetType());
+                }
+                else if (left as PartialSqlString == null && right as PartialSqlString == null)
+                {
+                    var result = Expression.Lambda(b).Compile().DynamicInvoke();
+                    return result;
+                }
+                else if (left as PartialSqlString == null)
+                    left = OrmLiteConfig.DialectProvider.GetQuotedValue(left, left != null ? left.GetType() : null);
+                else if (right as PartialSqlString == null)
+                    right = OrmLiteConfig.DialectProvider.GetQuotedValue(right, right != null ? right.GetType() : null);
+
+            }
+
+            if (operand == "=" && right.ToString() == "null") operand = "is";
+            else if (operand == "<>" && right.ToString() == "null") operand = "is not";
+            else if (operand == "=" || operand == "<>")
+            {
+                if (IsTrueExpression(right)) right = GetQuotedTrueValue();
+                else if (IsFalseExpression(right)) right = GetQuotedFalseValue();
+
+                if (IsTrueExpression(left)) left = GetQuotedTrueValue();
+                else if (IsFalseExpression(left)) left = GetQuotedFalseValue();
+
+            }
+
+            switch (operand)
+            {
+                case "MOD":
+                case "COALESCE":
+                    return new PartialSqlString(string.Format("{0}({1},{2})", operand, left, right));
+                default:
+                    return new PartialSqlString("(" + left + Sep + operand + Sep + right + ")");
+            }
+        }
+
+        private bool IsTrueExpression(object exp)
+        {
+            return (exp.ToString() == _trueExpression);
+        }
+
+	    private bool IsFalseExpression(object exp)
+        {
+            return (exp.ToString() == _falseExpression);
+        }
+
 	    public override string LimitExpression{
 			get{
 				if(!Skip.HasValue) return "";
