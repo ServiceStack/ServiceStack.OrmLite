b42f9f1bb3e4d233f25e99f077afb35467e61adc
Thomas Grassauer <thomas.grassauer@counity.at>
2012-09-27 11:18:15 +0200

    fixed update bug

--- src/ServiceStack.OrmLite.SqlServer/SqlServerExpressionVisitor.cs
+++ src/ServiceStack.OrmLite.SqlServer/SqlServerExpressionVisitor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using ServiceStack.Text;
 
 namespace ServiceStack.OrmLite.SqlServer
 {
@@ -50,6 +52,30 @@ namespace ServiceStack.OrmLite.SqlServer
 	            skip + take);
 
 	        return retVal;
+        }
+
+        public override string ToUpdateStatement(T item, bool excludeDefaults = false)
+        {
+
+            var setFields = new StringBuilder();
+            var dialectProvider = OrmLiteConfig.DialectProvider;
+
+            foreach (var fieldDef in ModelDef.FieldDefinitions)
+            {
+                if (UpdateFields.Count > 0 && !UpdateFields.Contains(fieldDef.Name) || fieldDef.AutoIncrement) continue; // added
+                var value = fieldDef.GetValue(item);
+                if (excludeDefaults && (value == null || value.Equals(value.GetType().GetDefaultValue()))) continue; //GetDefaultValue?
+
+                fieldDef.GetQuotedValue(item);
+
+                if (setFields.Length > 0) setFields.Append(",");
+                setFields.AppendFormat("{0} = {1}",
+                    dialectProvider.GetQuotedColumnName(fieldDef.FieldName),
+                    dialectProvider.GetQuotedValue(value, fieldDef.FieldType));
+            }
+
+            return string.Format("UPDATE {0} SET {1} {2}",
+                                                dialectProvider.GetQuotedTableName(ModelDef), setFields, WhereExpression);
         }
 
         protected virtual void AssertValidSkipRowValues()
