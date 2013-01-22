8a855abaadc4099680397ed8e66b65f4b64ed299
AkosLukacs <AkosLukacs42@gmail.com>
2013-01-22 15:12:55 +0100

    There is a bug in OrmLite.Oracle's paging sql generation, when there is no default ordering: OracleOrmLiteDialectProvider.GetColumnNames inserts an extra space between the select, and the first column.
    Added Oracle test project, and moved the command line tests out to a different directory.
    
    OracleSyntaxTests.cs contains failing tests.
    
    For example, this original query:
      SELECT  Id, Foo, Bar, Baz, "COMMENT"
      FROM TestEntity
    
    becomes this:
    
      SELECT * FROM (
      SELECT "_ss_ormlite_1_".*, ROWNUM RNUM FROM (
      SELECT  Id, Foo, Bar, Baz, "COMMENT"
      FROM TestEntity ORDER BY I) "_ss_ormlite_1_"   //ORDER BY "I", not by "Id" as it should be
      WHERE ROWNUM <= 10 + 100) "_ss_ormlite_2_" WHERE "_ss_ormlite_2_".RNUM > 10
    
    and you get an OracleException : ORA-00904: "I": invalid identifier

---
+++ src/ServiceStack.OrmLite.Oracle.Tests/OracleTestBase.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Configuration;
+using System.IO;
+using NUnit.Framework;
+using ServiceStack.Common.Utils;
+using ServiceStack.Logging;
+using ServiceStack.Logging.Support.Logging;
+using ServiceStack.OrmLite.Oracle;
+
+
+namespace ServiceStack.OrmLite.Oracle.Tests
+{
+    public class OracleTestBase
+	{
+		protected virtual string ConnectionString { get; set; }
+
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp()
+		{
+			LogManager.LogFactory = new ConsoleLogFactory();
+
+            OrmLiteConfig.DialectProvider = OracleOrmLiteDialectProvider.Instance;
+			OrmLiteConfig.ClearCache();
+		    ConnectionString = ConfigurationManager.ConnectionStrings["testDb"].ConnectionString;
+		}
+	}
+}
