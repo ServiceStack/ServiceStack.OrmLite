37e28a0e4e464632870092a065b058ca0caf0d07
adebisi-fa-ibm <adebisi-fa@live.com>
2013-03-04 15:00:04 +0100

    Replaces references to 'ServiceStack.Common.Extensions' namespace (which is now obsolete) with 'ServiceStack.Common' namespace.

--- src/FirebirdTests/TestLiteFirebird04/Main.cs
+++ src/FirebirdTests/TestLiteFirebird04/Main.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 using ServiceStack.Common.Utils;
 using ServiceStack.DataAnnotations;
-using ServiceStack.Common.Extensions;
+using ServiceStack.Common;
 
 using ServiceStack.OrmLite;
 using ServiceStack.OrmLite.Firebird;
