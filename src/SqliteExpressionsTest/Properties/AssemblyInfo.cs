65df39d1d3bfa226254c170c51c9196bc0f167c6
Nick Jones <medianick@gmail.com>
2013-04-08 12:26:42 -0400

    Switch to GlobalAssemblyInfo.cs for AssemblyVersion.
    All projects under src with a Properties\AssemblyInfo.cs file reference
    now reference a common GlobalAssemblyInfo.cs file under src, which
    specifies the AssemblyVersion attribute value.

--- src/SqliteExpressionsTest/Properties/AssemblyInfo.cs
+++ src/SqliteExpressionsTest/Properties/AssemblyInfo.cs
@@ -22,10 +22,3 @@ using System.Runtime.InteropServices;
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
 
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
