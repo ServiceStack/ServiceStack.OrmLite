5d8c9da1a042bc0de303b1fc2df9df004f67a98d
madaleno <madaleno@magnisoft.com>
2013-10-10 13:00:40 +0100

    Force Firebird to use UpperCaseNamingStategy
    FireBird works better if we use table and field names in upper case.
    If we have a poco named User, we can easily get in trouble with a query
    like this: select * from User;
    
    Forcing the FirebirdOrmLiteDialectProvider to use the
    UpperCaseNamingStrategy, this problem doesn't exist.

---
+++ src/ServiceStack.OrmLite/UpperCaseNamingStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceStack.OrmLite
+{
+    public class UpperCaseNamingStrategy: OrmLiteNamingStrategyBase
+	{
+		public virtual string GetTableName(string name)
+		{
+			return name.ToUpper();
+		}
+ 
+		public virtual string GetColumnName(string name)
+		{
+			return name.ToUpper();
+		}
+	}
+}
