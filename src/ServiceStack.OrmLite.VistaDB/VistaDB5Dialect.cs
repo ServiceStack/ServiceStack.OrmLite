705e95612f6e26faa7b54dad34bfdb29be96bdb4
Ilya Lukyanov <ilya.lukyanov@gmail.com>
2014-03-09 23:17:44 +0400

    VistaDB4 provider cloned to VistaDB5 provider
    Also added separate tests project

---
+++ src/ServiceStack.OrmLite.VistaDB/VistaDB5Dialect.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack.OrmLite.VistaDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceStack.OrmLite
+{
+    public static class VistaDB5Dialect
+    {
+        public static IOrmLiteDialectProvider Provider { get { return VistaDB5DialectProvider.Instance; } }
+    }
+}
