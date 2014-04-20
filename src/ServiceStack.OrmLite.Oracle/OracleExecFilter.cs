71e14c04c836f7b8666e4d5996bbcc4225d30127
Bruce Cowan <bruce.cowan@aquaticinformatics.com>
2014-04-20 13:13:53 -0700

    Write test to demonstrate Oracle provider problem with profiler and fix provider.

--- src/ServiceStack.OrmLite.Oracle/OracleExecFilter.cs
+++ src/ServiceStack.OrmLite.Oracle/OracleExecFilter.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace ServiceStack.OrmLite.Oracle
 {
@@ -9,16 +12,37 @@ namespace ServiceStack.OrmLite.Oracle
         {
             var command = base.CreateCommand(dbConn);
 
-            // Doing comparison and set this way to avoid having a reference to the Oracle client
-            // so that customers can use a different version than we used to compile without
-            // requiring a version redirect in a config file somewhere.
-            var commandType = command.GetType();
-            if (commandType.FullName.StartsWith("Oracle.DataAccess.Client", StringComparison.InvariantCulture))
-            {
-                var pi = commandType.GetProperty("BindByName");
-                pi.SetValue(command, true, null);
-            }
+            var action = GetBindByNameSetter(command.GetType());
+            if (action != null) action(command, true);
+
             return command;
+        }
+
+        private static readonly Dictionary<Type, Action<IDbCommand, bool>> Cache = new Dictionary<Type, Action<IDbCommand, bool>>();
+        private static Action<IDbCommand, bool> GetBindByNameSetter(Type commandType)
+        {
+            if (commandType == null) return null;
+
+            Action<IDbCommand, bool> action;
+            if (Cache.TryGetValue(commandType, out action)) return action;
+
+            var prop = commandType.GetProperty("BindByName", BindingFlags.Public | BindingFlags.Instance);
+            MethodInfo setter;
+            if (prop != null && prop.CanWrite && prop.PropertyType == typeof(bool)
+                && prop.GetIndexParameters().Length == 0 && (setter = prop.GetSetMethod()) != null)
+            {
+                var methodName = commandType.GetOperationName() + "_BindByName";
+                var method = new DynamicMethod(methodName, null, new []{ typeof(IDbCommand), typeof(bool) });
+                var il = method.GetILGenerator();
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Castclass, commandType);
+                il.Emit(OpCodes.Ldarg_1);
+                il.EmitCall(OpCodes.Callvirt, setter, null);
+                il.Emit(OpCodes.Ret);
+                action = (Action<IDbCommand, bool>)method.CreateDelegate(typeof(Action<IDbCommand, bool>));
+            }
+            Cache.Add(commandType, action);
+            return action;
         }
     }
 }
