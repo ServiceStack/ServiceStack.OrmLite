a2e2fff198a10ec32e9d1f69faf941e953783d46
jessemcdowell-AI <jesse.mcdowell@aquaticinformatics.com>
2014-05-21 15:24:35 -0700

    Add unit tests

---
+++ tests/ServiceStack.OrmLite.Tests/OptimisticConcurrencyTests.cs
@@ -0,0 +1,116 @@
+﻿using System.Data;
+using NUnit.Framework;
+using ServiceStack.DataAnnotations;
+
+namespace ServiceStack.OrmLite.Tests
+{
+    [TestFixture]
+    public class OptimisticConcurrencyTests 
+        : OrmLiteTestBase
+    {
+        private IDbConnection db;
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            using (var dbConn = OpenDbConnection())
+            {
+                dbConn.DropAndCreateTable<ModelWithRowVersion>();                
+            }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            db = OpenDbConnection();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            db.Dispose();
+        }
+
+        [Test]
+        public void Select_retrieves_rowversion()
+        {
+            var rowId = db.Insert(new ModelWithRowVersion { Text = "One" }, selectIdentity:true);
+            TouchRow(rowId);
+
+            var row = db.SingleById<ModelWithRowVersion>(rowId);
+
+            Assert.That(row.Version, Is.Not.EqualTo(0));
+        }
+
+        [Test]
+        public void Can_update_with_current_rowversion()
+        {
+            var rowId = db.Insert(new ModelWithRowVersion { Text = "Two" }, selectIdentity: true);
+            var row = db.SingleById<ModelWithRowVersion>(rowId);
+
+            row.Text = "Three";
+            db.Update(row);
+
+            var actual = db.SingleById<ModelWithRowVersion>(rowId);
+            Assert.That(actual.Text, Is.EqualTo("Three"));
+            Assert.That(actual.Version, Is.Not.EqualTo(row.Version));
+        }
+
+        [Test]
+        public void Can_delete_with_current_rowversion()
+        {
+            var rowId = db.Insert(new ModelWithRowVersion { Text = "Four" }, selectIdentity: true);
+            var row = db.SingleById<ModelWithRowVersion>(rowId);
+
+            db.Delete(row);
+
+            var count = db.Count<ModelWithRowVersion>(m => m.Id == rowId);
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Update_with_outdated_rowversion_throws()
+        {
+            var rowId = db.Insert(new ModelWithRowVersion { Text = "Five" }, selectIdentity: true);
+            var row = db.SingleById<ModelWithRowVersion>(rowId);
+            TouchRow(rowId);
+
+            row.Text = "Six";
+            Assert.Throws<RowModifiedException>(() => db.Update(row));
+
+            var actual = db.SingleById<ModelWithRowVersion>(rowId);
+            Assert.That(actual.Text, Is.Not.EqualTo("Six"));
+        }
+
+        [Test]
+        public void Delete_with_outdated_rowversion_throws()
+        {
+            var rowId = db.Insert(new ModelWithRowVersion { Text = "Seven" }, selectIdentity: true);
+            var row = db.SingleById<ModelWithRowVersion>(rowId);
+            TouchRow(rowId);
+
+            Assert.Throws<RowModifiedException>(() => db.Delete(row));
+
+            var count = db.Count<ModelWithRowVersion>(m => m.Id == rowId);
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        private void TouchRow(long rowId)
+        {
+            var row = db.SingleById<ModelWithRowVersion>(rowId);
+            row.Text = "Touched";
+            db.Update(row);
+        }
+    }
+
+    public class ModelWithRowVersion
+    {
+        [AutoIncrement]
+        public long Id { get; set; }
+
+        public string Text { get; set; }
+
+        [RowVersion]
+        public long Version { get; set; }
+    }
+}
