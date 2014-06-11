1b4c76c3dbeb546c33a9d9ec3bf0620f292aea9b
Demis Bellot <demis.bellot@gmail.com>
2014-06-10 20:14:29 -0400

    Refactor PostgreSqlExpression to use DialectProvider

--- src/ServiceStack.OrmLite.PostgreSQL/PostgreSqlExpression.cs
+++ src/ServiceStack.OrmLite.PostgreSQL/PostgreSqlExpression.cs
@@ -2,24 +2,6 @@ namespace ServiceStack.OrmLite.PostgreSQL
 {
     public class PostgreSqlExpression<T> : SqlExpression<T>
     {
-        public override string LimitExpression
-        {
-            get
-            {
-                if (!Rows.HasValue) return "";
-                string offset;
-                if (Offset.HasValue)
-                {
-                    offset = string.Format(" OFFSET {0}", Offset.Value);
-                }
-                else
-                {
-                    offset = string.Empty;
-                }
-                return string.Format("LIMIT {0}{1}", Rows.Value, offset);
-            }
-        }
-
         public override SqlExpression<T> Clone()
         {
             return CopyTo(new PostgreSqlExpression<T>());
