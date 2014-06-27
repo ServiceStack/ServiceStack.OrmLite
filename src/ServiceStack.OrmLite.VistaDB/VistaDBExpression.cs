e813df340199be28423cd665842a6e6b6c53d05c
Ilya Lukyanov <ilya.lukyanov@gmail.com>
2014-06-27 16:13:26 +0400

    Fixed tests + refactoring

--- src/ServiceStack.OrmLite.VistaDB/VistaDBExpression.cs
+++ src/ServiceStack.OrmLite.VistaDB/VistaDBExpression.cs
@@ -36,43 +36,6 @@ namespace ServiceStack.OrmLite.VistaDB
                 dialectProvider.GetQuotedTableName(ModelDef), setFields, WhereExpression);
         }
 
-        protected virtual string BuildOrderByIdExpression()
-        {
-            if (ModelDef.PrimaryKey == null)
-                throw new ApplicationException("Malformed model, no PrimaryKey defined");
-
-            return String.Format("ORDER BY {0}", OrmLiteConfig.DialectProvider.GetQuotedColumnName(ModelDef.PrimaryKey.FieldName));
-        }
-
-        protected virtual string BuildWhereIdGTEFirstId()
-        {
-            if (ModelDef.PrimaryKey == null)
-                throw new ApplicationException("Malformed model, no PrimaryKey defined");
-
-            var where = String.IsNullOrWhiteSpace(WhereExpression)
-                ? String.Format("{0} > @first_id", OrmLiteConfig.DialectProvider.GetQuotedColumnName(ModelDef.PrimaryKey.FieldName))
-                : String.Format("({0}) AND {1} > @first_id", WhereExpression.Trim().Substring("WHERE".Length), OrmLiteConfig.DialectProvider.GetQuotedColumnName(ModelDef.PrimaryKey.FieldName));
-            
-            return "WHERE " + where;
-        }
-
-        protected virtual string BuildSelectTopNIdExpression(int skip)
-        {
-            if (ModelDef.PrimaryKey == null)
-                throw new ApplicationException("Malformed model, no PrimaryKey defined");
-            
-            return String.Format("SELECT TOP {0} @first_id = {1} ", skip, OrmLiteConfig.DialectProvider.GetQuotedColumnName(ModelDef.PrimaryKey.FieldName));
-        }
-
-        protected virtual string BuildDeclareFirstIdExpression()
-        {
-            if (ModelDef.PrimaryKey == null)
-                throw new ApplicationException("Malformed model, no PrimaryKey defined");
-
-            return String.Format("DECLARE @first_id {0};",
-                OrmLiteConfig.DialectProvider.GetColumnTypeDefinition(ModelDef.PrimaryKey.FieldType));
-        }
-
         protected override object VisitColumnAccessMethod(MethodCallExpression m)
         {
             if (m.Arguments.Count == 1 && m.Method.Name == "Equals")
