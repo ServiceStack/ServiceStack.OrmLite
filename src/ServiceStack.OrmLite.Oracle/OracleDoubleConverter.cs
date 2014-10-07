76388f519e096c89b5741bd93a713b1ad93f3a37
jeffjung-AI <jeff.jung@aquaticinformatics.com>
2014-10-07 11:41:04 -0700

    Fix OracleOrmLiteDialectProvider to read large double values correctly

---
+++ src/ServiceStack.OrmLite.Oracle/OracleDoubleConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Globalization;
+using System.Reflection;
+
+namespace ServiceStack.OrmLite.Oracle
+{
+    public class OracleDoubleConverter
+    {
+        public Assembly OracleAssembly { get; set; }
+        private MethodInfo GetOracleValue { get; set; }
+
+        public OracleDoubleConverter(DbProviderFactory factory)
+        {
+            OracleAssembly = factory.GetType().Assembly;
+            var readerType = OracleAssembly.GetType("Oracle.DataAccess.Client.OracleDataReader");
+            GetOracleValue = readerType.GetMethod("GetOracleValue", BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        public double ConvertToDouble(IDataReader dataReader, int colIndex)
+        {
+            object value;
+            if (GetOracleValue == null)
+                value = dataReader.GetValue(colIndex);
+            else
+                value = GetOracleValue.Invoke(dataReader, new object[] {colIndex});
+            return double.Parse(value.ToString(), CultureInfo.InvariantCulture);
+        }
+    }
+}
