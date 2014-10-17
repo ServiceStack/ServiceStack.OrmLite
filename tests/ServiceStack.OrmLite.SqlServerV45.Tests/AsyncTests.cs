0e7824cc330bf4d729259587c1f6e98071f44c50
Demis Bellot <demis.bellot@gmail.com>
2014-10-16 21:51:15 -0400

    Add new SqlServer .NET 4.5 project to test SqlServer async support

---
+++ tests/ServiceStack.OrmLite.SqlServerV45.Tests/AsyncTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ServiceStack.Common.Tests.Models;
+using ServiceStack.OrmLite.Async;
+using ServiceStack.Text;
+
+namespace ServiceStack.OrmLite.SqlServerV45.Tests
+{
+    [TestFixture]
+    public class AsyncTests
+        : OrmLiteTestBase
+    {
+        [Test]
+        public async Task Can_SelectAsync()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<Poco>();
+
+                3.Times(x =>
+                    db.Insert(new Poco { Name = x.ToString() }));
+
+                var results = await db.SelectAsync<Poco>();
+                results.PrintDump();
+            }
+        }
+    }
+
+}
---
+++ tests/ServiceStack.OrmLite.SqlServerV45.Tests/OrmLiteTestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using NUnit.Framework;
+using ServiceStack.Logging;
+
+namespace ServiceStack.OrmLite.SqlServerV45.Tests
+{
+    public class OrmLiteTestBase
+    {
+        protected virtual string ConnectionString { get; set; }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            LogManager.LogFactory = new ConsoleLogFactory();
+
+            OrmLiteConfig.DialectProvider = SqlServerDialect.Provider;
+            ConnectionString = ConfigurationManager.ConnectionStrings["testDb"].ConnectionString;
+        }
+
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public virtual IDbConnection OpenDbConnection(string connString = null)
+        {
+            connString = connString ?? ConnectionString;
+            return connString.OpenDbConnection();
+        }
+    }
+}
