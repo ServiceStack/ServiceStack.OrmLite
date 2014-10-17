253ce66d4c5906167bfc8cf6472cd9472a5f4975
Demis Bellot <demis.bellot@gmail.com>
2014-10-16 21:48:23 -0400

    Missing .NET 4.0 Task utils

---
+++ src/ServiceStack.OrmLite/Async/AsyncUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceStack.OrmLite
+{
+    public static class AsyncUtils
+    {
+        public static Task<T> FromResult<T>(T result)
+        {
+            var taskSource = new TaskCompletionSource<T>();
+            taskSource.SetResult(result);
+            return taskSource.Task;
+        }
+
+        public static Task<To> Cast<From, To>(this Task<From> task) where To : From
+        {
+            return task.Then(x => (To) x);
+        }
+
+        public static Task<To> Then<From, To>(this Task<From> task, Func<From, To> fn)
+        {
+            var tcs = new TaskCompletionSource<To>();
+            task.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                    tcs.TrySetException(t.Exception.InnerExceptions);
+                else if (t.IsCanceled)
+                    tcs.TrySetCanceled();
+                else
+                    tcs.TrySetResult(fn(t.Result));
+            }, TaskContinuationOptions.ExecuteSynchronously);
+
+            return tcs.Task;
+        }
+    }
+}
