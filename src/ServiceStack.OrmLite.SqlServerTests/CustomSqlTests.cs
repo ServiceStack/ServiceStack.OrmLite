3609adc39bbf5bba4e6d6c4053d20c7eb9361461
jeffjung-AI <jeff.jung@aquaticinformatics.com>
2014-11-11 13:22:31 -0800

    Demonstrate System.InvalidOperationException caused by the SqlList and SqlProc APIs

---
+++ src/ServiceStack.OrmLite.SqlServerTests/CustomSqlTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Data;
+using NUnit.Framework;
+using ServiceStack.DataAnnotations;
+
+namespace ServiceStack.OrmLite.SqlServerTests
+{
+    public class LetterFrequency
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public string Letter { get; set; }
+    }
+
+    [TestFixture]
+    public class CustomSqlTests : OrmLiteTestBase
+    {
+        private const string DropProcedureSql = @"
+            IF OBJECT_ID('spSearchLetters') IS NOT NULL
+                    DROP PROCEDURE spSearchLetters";
+
+        private const string CreateProcedureSql = @"
+            CREATE PROCEDURE spSearchLetters 
+            (
+                @pLetter varchar(10),
+                @pTotal int OUT
+            )
+            AS
+            BEGIN
+                SELECT @pTotal = COUNT(*) FROM LetterFrequency WHERE Letter = @pLetter
+                SELECT * FROM LetterFrequency WHERE Letter = @pLetter
+            END";
+
+        [Test]
+        public void Can_execute_stored_procedure_using_SqlList_with_out_params()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<LetterFrequency>();
+
+                var rows = "A,B,B,C,C,C,D,D,E".Split(',').Map(x => new LetterFrequency { Letter = x });
+                db.InsertAll(rows);
+
+                db.ExecuteSql(DropProcedureSql);
+                db.ExecuteSql(CreateProcedureSql);
+
+                IDbDataParameter pTotal = null;
+                var results = db.SqlList<LetterFrequency>("spSearchLetters",
+                    cmd =>
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.AddParam("pLetter", "C");
+                        // Replace above line with the following two lines
+                        // var pLetter = cmd.AddParam("pLetter", "C");
+                        // pLetter.Size = 10;
+                        pTotal = cmd.AddParam("pTotal", direction: ParameterDirection.Output);
+                        // Add the following line
+                        // pTotal.Size = 10;
+                    });
+
+                Assert.That(results.Count, Is.EqualTo(3));
+                Assert.That(pTotal.Value, Is.EqualTo("3"));
+            }
+        }
+
+        [Test]
+        public void Can_execute_stored_procedure_using_SqlProc_with_out_params()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<LetterFrequency>();
+
+                var rows = "A,B,B,C,C,C,D,D,E".Split(',').Map(x => new LetterFrequency { Letter = x });
+                db.InsertAll(rows);
+
+                db.ExecuteSql(DropProcedureSql);
+                db.ExecuteSql(CreateProcedureSql);
+
+                var cmd = db.SqlProc("spSearchLetters", new { pLetter = "C" });
+                // Add the following two lines
+                // var pLetter = (IDbDataParameter)cmd.Parameters["pLetter"];
+                // pLetter.Size = 10;
+                var pTotal = cmd.AddParam("pTotal", direction: ParameterDirection.Output);
+                // Add the following line
+                // pTotal.Size = 10;
+                var results = cmd.ConvertToList<LetterFrequency>();
+
+                Assert.That(results.Count, Is.EqualTo(3));
+                Assert.That(pTotal.Value, Is.EqualTo("3"));
+            }
+        }
+    }
+}
