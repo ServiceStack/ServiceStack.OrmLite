94ff51b0d69c8e24e51bbbac44f0b50269686346
Demis Bellot <demis.bellot@gmail.com>
2015-01-08 06:59:01 -0500

    Change to Dispose datareader in a future callback in async API's

--- tests/ServiceStack.OrmLiteV45.Tests/SelectAsyncTests.cs
+++ tests/ServiceStack.OrmLiteV45.Tests/SelectAsyncTests.cs
@@ -1,9 +1,22 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using AppDb;
 using NUnit.Framework;
+using ServiceStack.DataAnnotations;
+using ServiceStack.Model;
 
 namespace ServiceStack.OrmLite.Tests
 {
+    public class PocoWithBytes : IHasGuidId
+    {
+        [PrimaryKey]
+        public Guid Id { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string ContentType { get; set; }
+    }
+
     [TestFixture]
     public class SelectAsyncTests
         : OrmLiteTestBase
@@ -20,6 +33,21 @@ namespace ServiceStack.OrmLite.Tests
                 var row = await db.SingleAsync(db.From<Poco>().Where(x => x.Id == "foo"));
 
                 Assert.That(row.Id, Is.EqualTo("foo"));
+            }
+        }
+
+        [Test]
+        public async Task Can_SELECT_SingleAsyncForStrangeClass()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<PocoWithBytes>();
+
+                var bar = new PocoWithBytes { Id = Guid.NewGuid(), Image = new byte[1024 * 1024], ContentType = "image/jpeg" };
+                await db.InsertAsync(bar);
+
+                var blah = await db.SingleAsync(db.From<PocoWithBytes>().Where(x => x.Id == bar.Id));
+                Assert.That(blah, Is.Not.Null);
             }
         }
     }
