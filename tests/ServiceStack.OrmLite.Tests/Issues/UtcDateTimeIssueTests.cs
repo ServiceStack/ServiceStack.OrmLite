a71b25bfa95b208d4c3c90f82fbdeffc4bf37ff1
Demis Bellot <demis.bellot@gmail.com>
2015-02-02 16:12:06 -0500

    Add UtcDateTimeIssueTests

---
+++ tests/ServiceStack.OrmLite.Tests/Issues/UtcDateTimeIssueTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ServiceStack.OrmLite.Tests.Issues
+{
+    public class TestDate
+    {
+        public string Name { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+
+    [TestFixture]
+    public class UtcDateTimeIssueTests : OrmLiteTestBase
+    {
+        [Test]
+        public void Test_DateTime_Select()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<TestDate>();
+
+                db.Insert(new TestDate {
+                    Name = "Test name", 
+                    ExpiryDate = DateTime.UtcNow.AddHours(1)
+                });
+
+                var result = db.Select<TestDate>(q => q.ExpiryDate > DateTime.UtcNow);
+
+                Assert.That(result.Count, Is.EqualTo(1));
+            }
+        }         
+    }
+}
