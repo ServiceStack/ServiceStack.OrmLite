7f01bd2ecc27b8f2765bfc52c461f4d6fe0cadeb
Kevin Howard <kevin@gunrun.net>
2015-08-19 15:16:00 -0400

    Added test project demoing the use of Spatials
    Added new test project demonstrating the use of Spatials (both Geometry
    and Geography) types with OrmLite.  This functionality depends on the
    Microsoft.SqlServer.Types library from NuGet, plus references to the
    unmanaged libraries msvcr100.dll & SqlServerSpatials110.dll.

---
+++ src/ServiceStack.OrmLite.SqlServerSpatialsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServiceStack.OrmLite.SqlServerTests.Spatials")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ServiceStack")]
+[assembly: AssemblyProduct("ServiceStack.OrmLite.SqlServerTests.Spatials")]
+[assembly: AssemblyCopyright("Copyright © ServiceStack 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b366503-ecfc-4783-afb6-900f1364ea7b")]
+
---
+++ src/ServiceStack.OrmLite.SqlServerSpatialsTest/SqlServerGeometryTypeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using ServiceStack.OrmLite;
+
+namespace ServiceStack.OrmLite.SqlServer.Converters
+{
+    public class SqlServerGeometryTypeConverter : OrmLiteConverter
+    {
+        public override string ColumnDefinition
+        {
+            get { return "GEOMETRY"; }
+        }
+
+        public override DbType DbType
+        {
+            get { return DbType.Object; }
+        }
+    }
+}
