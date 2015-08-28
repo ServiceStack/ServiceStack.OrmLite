6afb30148ff3a5027bb00085ddb65f1766c9995e
Demis Bellot <demis.bellot@gmail.com>
2015-08-27 22:42:35 -0700

    Add test for serializing dapper dynamic results to JSON and CSV

---
+++ tests/ServiceStack.OrmLite.Tests/Issues/CsvTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ServiceStack.Common.Tests.Models;
+using ServiceStack.OrmLite.Dapper;
+using ServiceStack.Text;
+
+namespace ServiceStack.OrmLite.Tests.Issues
+{
+    [TestFixture]
+    public class CsvTests : OrmLiteTestBase
+    {
+        [Test]
+        public void Can_serialize_Dapper_results_to_CSV()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<Poco>();
+
+                db.Insert(new Poco { Id = 1, Name = "Foo" });
+                db.Insert(new Poco { Id = 2, Name = "Bar" });
+
+                var results = db.Query("select * from Poco");
+
+                var json = JsonSerializer.SerializeToString(results);
+                Assert.That(json, Is.EqualTo("[{\"Id\":1,\"Name\":\"Foo\"},{\"Id\":2,\"Name\":\"Bar\"}]"));
+
+                var csv = CsvSerializer.SerializeToCsv(results);
+                Assert.That(csv.NormalizeNewLines(), Is.EqualTo("Id,Name\n1,Foo\n2,Bar\n"));
+            }
+        }
+    }
+}
