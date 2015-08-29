a5a489d462a6c451c31e27a99d41e9c0499b528a
Demis Bellot <demis.bellot@gmail.com>
2015-08-29 12:45:00 -0700

    Add OrmLite example of CSV serialization

--- tests/ServiceStack.OrmLite.Tests/Issues/CsvTests.cs
+++ tests/ServiceStack.OrmLite.Tests/Issues/CsvTests.cs
@@ -28,5 +28,29 @@ namespace ServiceStack.OrmLite.Tests.Issues
                 Assert.That(csv.NormalizeNewLines(), Is.EqualTo("Id,Name\n1,Foo\n2,Bar\n"));
             }
         }
+
+        [Test]
+        public void Can_serialize_OrmLite_results_to_CSV()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<Poco>();
+
+                db.Insert(new Poco { Id = 1, Name = "Foo" });
+                db.Insert(new Poco { Id = 2, Name = "Bar" });
+
+                var results = db.Select<Poco>();
+
+                var json = JsonSerializer.SerializeToString(results);
+                Assert.That(json, Is.EqualTo("[{\"Id\":1,\"Name\":\"Foo\"},{\"Id\":2,\"Name\":\"Bar\"}]"));
+
+                var csv = results.ToCsv();
+                Assert.That(csv.NormalizeNewLines(), Is.EqualTo("Id,Name\n1,Foo\n2,Bar\n"));
+
+                var rows = db.Select<Dictionary<string,object>>("select * from Poco");
+                csv = rows.ToCsv();
+                Assert.That(csv.NormalizeNewLines(), Is.EqualTo("Id,Name\n1,Foo\n2,Bar\n"));
+            }
+        }
     }
 }
