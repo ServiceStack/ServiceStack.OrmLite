e45b49bd37b4dc227a456fe0e16ad459afbb52bc
Demis Bellot <demis.bellot@gmail.com>
2015-12-23 19:53:21 -0800

    Add support for ParameterizedSqlExpression in Sqlite

--- src/ServiceStack.OrmLite.Sqlite/SqliteOrmLiteDialectProviderBase.cs
+++ src/ServiceStack.OrmLite.Sqlite/SqliteOrmLiteDialectProviderBase.cs
@@ -148,7 +148,9 @@ namespace ServiceStack.OrmLite.Sqlite
 
         public override SqlExpression<T> SqlExpression<T>()
         {
-            return new SqliteExpression<T>(this);
+            return !OrmLiteConfig.UseParameterizeSqlExpressions
+                ? new SqliteExpression<T>(this)
+                : (SqlExpression<T>)new SqliteParameterizedSqlExpression<T>(this);
         }
 
         public override bool DoesTableExist(IDbCommand dbCmd, string tableName, string schema = null)
