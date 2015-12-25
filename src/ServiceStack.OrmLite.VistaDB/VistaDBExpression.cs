6176038e6c3b1fd0bf6a2daddf0f98c5fb84825e
Demis Bellot <demis.bellot@gmail.com>
2015-12-25 14:00:28 -0800

    Change VistaDB to use ParameterizedSqlExpression + impl CreateParam

--- src/ServiceStack.OrmLite.VistaDB/VistaDBExpression.cs
+++ src/ServiceStack.OrmLite.VistaDB/VistaDBExpression.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 
 namespace ServiceStack.OrmLite.VistaDB
 {
-    public class VistaDbExpression<T> : SqlExpression<T>
+    public class VistaDbExpression<T> : ParameterizedSqlExpression<T>
     {
         public VistaDbExpression(IOrmLiteDialectProvider dialectProvider) 
             : base(dialectProvider) {}
@@ -13,7 +13,6 @@ namespace ServiceStack.OrmLite.VistaDB
         public override string ToUpdateStatement(T item, bool excludeDefaults = false)
         {
             var setFields = new StringBuilder();
-            var dialectProvider = OrmLiteConfig.DialectProvider;
 
             foreach (var fieldDef in ModelDef.FieldDefinitions)
             {
@@ -29,15 +28,15 @@ namespace ServiceStack.OrmLite.VistaDB
 
                 if (setFields.Length > 0) setFields.Append(",");
                 setFields.AppendFormat("{0} = {1}",
-                    dialectProvider.GetQuotedColumnName(fieldDef.FieldName),
-                    dialectProvider.GetQuotedValue(value, fieldDef.FieldType));
+                    DialectProvider.GetQuotedColumnName(fieldDef.FieldName),
+                    DialectProvider.GetQuotedValue(value, fieldDef.FieldType));
             }
 
             if (setFields.Length == 0)
                 throw new ArgumentException("No non-null or non-default values were provided for type: " + typeof(T).Name);
 
             return string.Format("UPDATE {0} SET {1} {2}",
-                dialectProvider.GetQuotedTableName(ModelDef), setFields, WhereExpression);
+                DialectProvider.GetQuotedTableName(ModelDef), setFields, WhereExpression);
         }
 
         protected override object VisitColumnAccessMethod(MethodCallExpression m)
