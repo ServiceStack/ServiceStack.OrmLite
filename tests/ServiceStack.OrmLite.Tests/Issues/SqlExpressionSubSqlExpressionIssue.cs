c0cfddac6a876620f7c849cd10bb61da0ec63206
Demis Bellot <demis.bellot@gmail.com>
2016-06-17 11:59:41 -0400

    Add test for using var in subExpr

--- tests/ServiceStack.OrmLite.Tests/Issues/SqlExpressionSubSqlExpressionIssue.cs
+++ tests/ServiceStack.OrmLite.Tests/Issues/SqlExpressionSubSqlExpressionIssue.cs
@@ -61,5 +61,48 @@ namespace ServiceStack.OrmLite.Tests.Issues
                 results.PrintDump();
             }
         }
+
+        public class Person2
+        {
+            [AutoIncrement]
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class Order2
+        {
+            [AutoIncrement]
+            public int Id { get; set; }
+
+            [References(typeof(Person2))]
+            public int Person2Id { get; set; }
+
+            public DateTime OrderDate { get; set; }
+
+            public int OrderTypeId { get; set; }
+        }
+
+        [Test]
+        public void Can_reference_variable_in_sub_expression()
+        {
+            int orderTypeId = 2;
+
+            using (var db = OpenDbConnection())
+            {
+                var subExpr = db.From<Order2>()
+                    .Where(y => y.OrderTypeId == orderTypeId)
+                    .Select(y => y.Person2Id);
+
+                subExpr.ToSelectStatement().Print();
+                Assert.That(subExpr.ToSelectStatement().NormalizeSql(), Is.StringContaining("@0"));
+
+                var expr = db.From<Person2>()
+                    .Where(x => Sql.In(x.Id, subExpr));
+
+                expr.ToSelectStatement().Print();
+                Assert.That(expr.ToSelectStatement().NormalizeSql(), Is.StringContaining("@0"));
+            }
+        }
     }
 }
