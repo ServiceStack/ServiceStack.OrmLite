f0174c5aecfbda1c155d457783d600fd768e6b98
Evgeny Morozov <evgeny.morozov@shift-technology.com>
2016-06-22 16:23:26 +0200

    Fixed tests in PostgreSQL/SchemaTests failing due to case differences

--- src/ServiceStack.OrmLite.PostgreSQL.Tests/SchemaTests.cs
+++ src/ServiceStack.OrmLite.PostgreSQL.Tests/SchemaTests.cs
@@ -53,10 +53,10 @@ $$;";
                 CreateSchemaIfNotExists(db);
                 db.DropAndCreateTable<User>();
 
-                var tables = db.Column<string>(@"SELECT '[' || n.nspname || '].[' || c.relname ||']' FROM pg_class c LEFT JOIN pg_namespace n ON n.oid = c.relnamespace WHERE c.relname = 'Users' AND n.nspname = 'TestSchema'");
+                var tables = db.Column<string>(@"SELECT '[' || n.nspname || '].[' || c.relname ||']' FROM pg_class c LEFT JOIN pg_namespace n ON n.oid = c.relnamespace WHERE c.relname = 'users' AND n.nspname = 'TestSchema'");
                 
                 //PostgreSQL dialect should create the table in the schema
-                Assert.That(tables.Contains("[TestSchema].[Users]"));
+                Assert.That(tables.Contains("[TestSchema].[users]"));
             }
         }
 
@@ -76,7 +76,7 @@ $$;";
                 var lastInsertId = db.LastInsertId();
                 Assert.That(lastInsertId, Is.GreaterThan(0));
 
-                var rowsB = db.Select<User>("\"Name\" = @name", new { name = "B" });
+                var rowsB = db.Select<User>("\"name\" = @name", new { name = "B" });
                 Assert.That(rowsB, Has.Count.EqualTo(2));
 
                 var rowIds = rowsB.ConvertAll(x => x.Id);
@@ -84,7 +84,7 @@ $$;";
 
                 rowsB.ForEach(x => db.Delete(x));
 
-                rowsB = db.Select<User>("\"Name\" = @name", new { name = "B" });
+                rowsB = db.Select<User>("\"name\" = @name", new { name = "B" });
                 Assert.That(rowsB, Has.Count.EqualTo(0));
 
                 var rowsLeft = db.Select<User>();
