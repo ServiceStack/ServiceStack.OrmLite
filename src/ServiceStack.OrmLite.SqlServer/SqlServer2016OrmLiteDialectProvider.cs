bc912624a4aa23422a1caa0308126b6c3a57ffeb
Kevin Howard <mindcore1@yahoo.com>
2016-11-19 15:19:27 -0500

    Added MemoryOptimized attribute to SqlServer provider

---
+++ src/ServiceStack.OrmLite.SqlServer/SqlServer2016OrmLiteDialectProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using ServiceStack.Text;
+
+namespace ServiceStack.OrmLite.SqlServer
+{
+    public class SqlServer2016OrmLiteDialectProvider : SqlServer2014OrmLiteDialectProvider
+    {
+    }
+}
