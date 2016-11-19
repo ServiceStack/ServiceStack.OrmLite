7b9443c2b8700ff88f61c651ae7e13b5eb0ae0cb
Kevin Howard <mindcore1@yahoo.com>
2016-11-19 15:19:27 -0500

    Added MemoryOptimized attribute to SqlServer provider

---
+++ src/ServiceStack.OrmLite.SqlServerTests/MemoryOptimizedAttributeTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using ServiceStack.DataAnnotations;
+
+namespace ServiceStack.OrmLite.SqlServerTests
+{
+    [TestFixture]
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            OrmLiteConfig.DialectProvider = SqlServer2014Dialect.Provider;
+
+            using (var dbConn = OpenDbConnection())
+            {
+                dbConn.DropTable<TypeWithMemTableNoDurability>();
+                dbConn.DropTable<TypeWithMemTableSchemaOnlyDurability>();
+                dbConn.DropTable<TypeWithMemTableSchemaAndDataDurability>();
+            }
+        }
+
+        [Test]
+        public void CanCreateMemoryOptimizedTable()
+        {
+            using (var dbConn = OpenDbConnection())
+            {
+                dbConn.CreateTable<TypeWithMemTableNoDurability>(true);
+            }
+        }
+
+        [Test]
+        public void CanCreateMemoryOptimizedTableWithSchemaOnlyDurability()
+        {
+            using (var dbConn = OpenDbConnection())
+            {
+                dbConn.CreateTable<TypeWithMemTableSchemaOnlyDurability>(true);
+            }
+        }
+
+        [Test]
+        public void CanCreateMemoryOptimizedTableWithSchemaAndDurability()
+        {
+            using (var dbConn = OpenDbConnection())
+            {
+                dbConn.CreateTable<TypeWithMemTableSchemaAndDataDurability>(true);
+            }
+        }
+    }
+    
+    public class TypeWithMemTableNoDurability
+        {
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class TypeWithMemTableSchemaOnlyDurability
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class TypeWithMemTableSchemaAndDataDurability
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
