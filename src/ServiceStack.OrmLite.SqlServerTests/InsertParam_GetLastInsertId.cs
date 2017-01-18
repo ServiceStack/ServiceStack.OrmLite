c575202fafdfac32794b346b4d827e03d0f8a845
Demis Bellot <demis.bellot@gmail.com>
2017-01-18 04:08:54 -0500

    Add SQL Server test to retrieve generated Server GUID

--- src/ServiceStack.OrmLite.SqlServerTests/InsertParam_GetLastInsertId.cs
+++ src/ServiceStack.OrmLite.SqlServerTests/InsertParam_GetLastInsertId.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite.Dapper;
 
 namespace ServiceStack.OrmLite.SqlServerTests
 {
@@ -27,8 +29,43 @@ namespace ServiceStack.OrmLite.SqlServerTests
             {
                 con.CreateTable<SimpleType>(true);
 
-                var lastInsertId = con.Insert(testObject, selectIdentity:true);
+                var lastInsertId = con.Insert(testObject, selectIdentity: true);
                 Assert.That(lastInsertId, Is.GreaterThan(0), "with InsertParam");
+            }
+        }
+
+        public class ServerGuid
+        {
+            [Default(typeof(Guid), "newid()")]
+            public Guid Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void Can_retrieve_ServerGuid()
+        {
+            using (var db = OpenDbConnection())
+            using (var cmd = db.CreateCommand())
+            {
+                db.DropAndCreateTable<ServerGuid>();
+
+                var obj = new ServerGuid { Name = "foo" };
+
+                cmd.GetDialectProvider().PrepareParameterizedInsertStatement<ServerGuid>(cmd,
+                    insertFields: OrmLiteUtils.GetNonDefaultValueInsertFields(obj));
+
+                cmd.CommandText = cmd.CommandText.Replace("VALUES", "OUTPUT inserted.Id VALUES");
+
+                cmd.GetDialectProvider().SetParameterValues<ServerGuid>(cmd, obj);
+
+                var id = (Guid)cmd.ExecuteScalar();
+
+                Assert.That(id, Is.Not.EqualTo(default(Guid)));
+
+                var insertedRow = db.SingleById<ServerGuid>(id);
+
+                Assert.That(insertedRow.Name, Is.EqualTo("foo"));
             }
         }
     }
