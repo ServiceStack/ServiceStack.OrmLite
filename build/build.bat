9088cf9d4b4c0808908fe86302b85a00c08dfecf
Demis Bellot <demis.bellot@gmail.com>
2017-04-07 02:02:09 -0400

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
-SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-%MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0;
-REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0
+%MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
+REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
