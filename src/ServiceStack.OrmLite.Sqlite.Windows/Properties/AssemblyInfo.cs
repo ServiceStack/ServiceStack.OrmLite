c496564916aa0529504481846726246cc96894a1
Demis Bellot <demis.bellot@gmail.com>
2017-04-06 20:28:49 -0400

    migrate OrmLite.Sqlite.Windows project + update NuGet projs

--- src/ServiceStack.OrmLite.Sqlite.Windows/Properties/AssemblyInfo.cs
+++ src/ServiceStack.OrmLite.Sqlite.Windows/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServiceStack.OrmLite.SqliteNet")]
+// [assembly: AssemblyProduct("ServiceStack.OrmLite.SqliteNet")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
--- src/ServiceStack.OrmLite.Sqlite32/SqliteDialect.cs
+++
@@ -1,9 +0,0 @@
-using ServiceStack.OrmLite.Sqlite;
-
-namespace ServiceStack.OrmLite
-{
-    public static class SqliteDialect
-    {
-        public static IOrmLiteDialectProvider Provider { get { return SqliteOrmLiteDialectProvider.Instance; } }
-    }
-}
