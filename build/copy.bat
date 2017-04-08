4cc1a4688f068f2552fd2bcdf6752857624b007c
Demis Bellot <demis.bellot@gmail.com>
2017-04-08 06:43:15 -0400

    Update copy.bat

--- build/copy.bat
+++ build/copy.bat
@@ -1,6 +1,5 @@
 REM SET BUILD=Debug
 SET BUILD=Release
-SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe 
 
 MD ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.SqlServer\lib
 MD ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.MySql\lib
@@ -9,22 +8,20 @@ MD ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Oracle\lib
 MD ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Firebird\lib
 MD ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.T4\content
 
-COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.SqlServer\lib
-
 COPY ..\src\T4\*.*  ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.T4\content
 
+COPY ..\src\ServiceStack.OrmLite\bin\%BUILD%\net45\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite\bin\%BUILD%\netstandard1.3\ServiceStack.OrmLite.* ..\..\ServiceStack\lib\netstandard1.3
+COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\net45\ServiceStack.OrmLite.Sqlite.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\netstandard1.3\ServiceStack.OrmLite.Sqlite.* ..\..\ServiceStack\lib\netstandard1.3
+COPY ..\src\ServiceStack.OrmLite.Sqlite.Windows\bin\%BUILD%\net45\ServiceStack.OrmLite.Sqlite.Windows.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\net45\ServiceStack.OrmLite.SqlServer.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\netstandard1.3\ServiceStack.OrmLite.SqlServer.* ..\..\ServiceStack\lib\netstandard1.3
+COPY ..\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\net45\ServiceStack.OrmLite.MySql.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\netstandard1.6\ServiceStack.OrmLite.MySql.* ..\..\ServiceStack\lib\netstandard1.6
+COPY ..\src\ServiceStack.OrmLite.PostgreSQL\bin\%BUILD%\net45\ServiceStack.OrmLite.PostgreSQL.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.OrmLite.PostgreSQL\bin\%BUILD%\netstandard1.3\ServiceStack.OrmLite.PostgreSQL.* ..\..\ServiceStack\lib\netstandard1.3
+COPY ..\src\ServiceStack.OrmLite.PostgreSQL\bin\%BUILD%\net45\Npgsql.* ..\..\ServiceStack\lib
 
-COPY ..\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.MySql\lib
-COPY ..\src\ServiceStack.OrmLite.PostgreSQL\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.PostgreSQL\lib
-COPY ..\src\ServiceStack.OrmLite.Oracle\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Oracle\lib
-COPY ..\src\ServiceStack.OrmLite.Firebird\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Firebird\lib
-COPY ..\src\ServiceStack.OrmLite.Firebird\bin\%BUILD%\FirebirdSql.Data.FirebirdClient.dll ..\..\ServiceStack.OrmLite\NuGet\ServiceStack.OrmLite.Firebird\lib
-
-COPY ..\src\ServiceStack.OrmLite.Sqlite.Windows\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.Windows* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.OrmLite.PostgreSQL\bin\%BUILD%\ServiceStack.OrmLite.* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.OrmLite.PostgreSQL\bin\%BUILD%\Npgsql.* ..\..\ServiceStack\lib
-
-COPY ..\src\ServiceStack.OrmLite\bin\Signed\ServiceStack.OrmLite.* ..\..\ServiceStack\lib\signed
-COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\Signed\ServiceStack.OrmLite.SqlServer.* ..\..\ServiceStack\lib\signed
+COPY ..\src\ServiceStack.OrmLite\bin\Signed\net45\ServiceStack.OrmLite.* ..\..\ServiceStack\lib\signed
+COPY ..\src\ServiceStack.OrmLite.SqlServer\bin\Signed\net45\ServiceStack.OrmLite.SqlServer.* ..\..\ServiceStack\lib\signed
