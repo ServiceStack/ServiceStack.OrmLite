aa17d0550fe7db8569301d91c3808defa0f53e36
Demis Bellot <demis.bellot@gmail.com>
2017-04-11 01:47:40 -0400

    refactor Signed build

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,13 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
+PUSHD ..\src
+dotnet restore ServiceStack.OrmLite.Signed.sln
+POPD
+
+%MSBUILD% build-sn.proj /target:NuGetPack /property:PatchVersion=0
+
+PUSHD ..\src
+dotnet restore ServiceStack.OrmLite.sln
+POPD
+
 %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
-REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0
