4ef776f207a39e07a9d190fb3538cd4f2043569d
Demis Bellot <demis.bellot@gmail.com>
2017-08-30 15:35:55 -0400

    update Metadata Test issue to exclude others + show initialization

--- tests/ServiceStack.OrmLite.Tests/Issues/MetadataPrimaryKeyIssue.cs
+++ tests/ServiceStack.OrmLite.Tests/Issues/MetadataPrimaryKeyIssue.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using NUnit.Framework;
 using ServiceStack.OrmLite.Tests.Expression;
 
@@ -11,32 +7,10 @@ namespace ServiceStack.OrmLite.Tests.Issues
     public class MetadataPrimaryKeyIssue : OrmLiteTestBase
     {
         [Test]
-        public void Should_return_same_model_reference()
-        {
-            var model1 = typeof(LetterFrequency).GetModelMetadata();
-            var model2 = typeof(LetterFrequency).GetModelMetadata();
-            Assert.AreEqual(model1, model2);
-        }
-
-        [Test]
-        public void Should_return_same_model_reference_multi_threaded()
-        {
-            Task<ModelDefinition> task1 = System.Threading.Tasks.Task.Run(() => typeof(LetterFrequency).GetModelMetadata());
-            Task<ModelDefinition> task2 = System.Threading.Tasks.Task.Run(() => typeof(LetterFrequency).GetModelMetadata());
-            System.Threading.Tasks.Task.WaitAll(task1, task2);
-
-            Assert.AreEqual(task1.Result, task2.Result);
-        }
-
-        [Test]
-        public void Should_generate_select_statement()
-        {
-            Assert.AreEqual(SelectStatement(), SelectStatement());
-        }
-
-        [Test]
         public void Should_generate_select_statement_multi_threaded()
         {
+            typeof(LetterFrequency).GetModelMetadata();
+
             Task<string> task1 = System.Threading.Tasks.Task.Run(() => SelectStatement());
             Task<string> task2 = System.Threading.Tasks.Task.Run(() => SelectStatement());
             System.Threading.Tasks.Task.WaitAll(task1, task2);
