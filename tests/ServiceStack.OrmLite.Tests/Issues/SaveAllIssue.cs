aeb4c6e9a6b282ba9d9f76b0836cb60fa6aa0be8
Demis Bellot <demis.bellot@gmail.com>
2017-09-20 19:41:49 -0400

    Add test showing SaveAll for single column tables

---
+++ tests/ServiceStack.OrmLite.Tests/Issues/SaveAllIssue.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace ServiceStack.OrmLite.Tests.Issues
+{
+    public class SaveAllIssue : OrmLiteTestBase
+    {
+        class UserRole
+        {
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void Can_use_SaveAll_to_save_one_column_table()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<UserRole>();
+
+                db.SaveAll(new[]
+                {
+                    new UserRole { Name = "Admin" },
+                    new UserRole { Name = "Reader" },
+                    new UserRole { Name = "Writer" },
+                });
+
+                var rows = db.Select<UserRole>();
+                Assert.That(rows.Map(x => x.Name), Is.EquivalentTo(new[]{ "Admin", "Reader", "Writer"}));
+            }
+        }
+    }
+}
