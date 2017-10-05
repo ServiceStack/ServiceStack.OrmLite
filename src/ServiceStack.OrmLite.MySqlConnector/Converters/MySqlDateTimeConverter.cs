bb8599618623b7e8f2f078dfb3392dbc5ef922ef
Simon Balarabe <sib@mitto.ch>
2017-10-05 12:27:05 +0200

    Create Shared Project for MySql type connectors; Introduce new Projects for new Dialect Providers;

---
+++ src/ServiceStack.OrmLite.MySqlConnector/Converters/MySqlDateTimeConverter.cs
@@ -0,0 +1,8 @@
+using ServiceStack.OrmLite.MySql.Converters;
+
+namespace ServiceStack.OrmLite.MySqlConnector.Converters
+{
+    public class MySqlConnectorDateTimeConverter : MySqlDateTimeConverterBase
+    {
+    }
+}
