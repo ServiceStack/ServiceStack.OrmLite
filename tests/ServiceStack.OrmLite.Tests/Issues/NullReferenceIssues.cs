b758af1e793261cf85bd53bddcb4ff5b55337575
Demis Bellot <demis.bellot@gmail.com>
2017-12-20 03:17:45 -0500

    Fix NRE with AlterColumn

---
+++ tests/ServiceStack.OrmLite.Tests/Issues/NullReferenceIssues.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using ServiceStack.Logging;
+
+namespace ServiceStack.OrmLite.Tests.Issues
+{
+    public class NullReferenceIssues : OrmLiteTestBase
+    {
+        public class Foo
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Key { get; set; }
+        }
+
+        [Test]
+        public void Can_AlterColumn()
+        {
+            if (Dialect == Dialect.Sqlite)
+                return; // Not supported
+
+            LogManager.LogFactory = new ConsoleLogFactory(debugEnabled:true);
+
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<Foo>();
+
+                db.AlterColumn(typeof(Foo), new FieldDefinition
+                {
+                    Name = "Name",
+                    FieldType = typeof(string),
+                    IsNullable = true,
+                    DefaultValue = null
+                });
+            }
+        }
+
+    }
+}
