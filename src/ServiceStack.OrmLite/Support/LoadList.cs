05c51c861d5425f9d6dda93a7be2caf8c54b8dca
Demis Bellot <demis.bellot@gmail.com>
2018-06-25 03:18:21 -0400

    Also remove limits in sub selects

--- src/ServiceStack.OrmLite/Support/LoadList.cs
+++ src/ServiceStack.OrmLite/Support/LoadList.cs
@@ -89,7 +89,7 @@ namespace ServiceStack.OrmLite.Support
             //Load Self Table.RefTableId PK
             var refQ = q.Clone();
             refQ.Select(dialectProvider.GetQuotedColumnName(modelDef, refSelf));
-            refQ.OrderBy(); //clear any ORDER BY in Sub Select's
+            refQ.OrderBy().ClearLimits(); //clear any ORDER BY or LIMIT's in Sub Select's
 
             var subSqlRef = refQ.ToMergedParamsSelectStatement();
 
