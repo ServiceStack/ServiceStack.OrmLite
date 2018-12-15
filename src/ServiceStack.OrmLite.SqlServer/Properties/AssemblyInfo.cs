2ad6c32206f96f366773955b34c6293aa2409f02
Demis Bellot <demis.bellot@gmail.com>
2018-12-15 19:06:41 +0800

    Refector to use Directory.Build.props + SourceLink + Embedded .pdbs + CI pkgs

--- src/ServiceStack.OrmLite.SqlServer/Properties/AssemblyInfo.cs
+++ src/ServiceStack.OrmLite.SqlServer/Properties/AssemblyInfo.cs
@@ -1,26 +1,3 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ServiceStack.OrmLite.SqlServer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ServiceStack.OrmLite.SqlServer")]
-[assembly: AssemblyCopyright("Copyright (c) ServiceStack 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("19f4a9ca-8076-4743-a4f3-e60708f24708")]
-
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("19f4a9ca-8076-4743-a4f3-e60708f24708")]
+[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
--- src/ServiceStack.OrmLite.Sqlite/Properties/AssemblyInfo.cs
+++ src/ServiceStack.OrmLite.Sqlite/Properties/AssemblyInfo.cs
@@ -1,25 +1,3 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ServiceStack.OrmLite.Sqlite")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright (c) ServiceStack 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ac105634-e91a-4fcc-a4f9-36aacd9fd47c")]
-
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("ac105634-e91a-4fcc-a4f9-36aacd9fd47c")]
+[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
