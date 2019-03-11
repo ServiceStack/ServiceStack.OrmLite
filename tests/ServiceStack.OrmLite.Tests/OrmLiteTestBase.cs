180ee75613b5c73dd4f88e3e46e960bf9509e352
Demis Bellot <demis.bellot@gmail.com>
2019-03-10 20:51:58 -0400

    Update connection string to look in Environment variables if exists

--- tests/ServiceStack.OrmLite.Tests/OrmLiteTestBase.cs
+++ tests/ServiceStack.OrmLite.Tests/OrmLiteTestBase.cs
@@ -16,23 +16,23 @@ namespace ServiceStack.OrmLite.Tests
         public static Dialect DefaultDialect = Dialect.Sqlite;
         public const bool EnableDebugLogging = false;
 
-        public static string SqliteMemoryDb = ":memory:";
+        public static string SqliteMemoryDb = Environment.GetEnvironmentVariable("SQLITE_CONNECTION") ?? ":memory:";
         public static string SqliteFileDir = "~/App_Data/".MapAbsolutePath();
         public static string SqliteFileDb = "~/App_Data/db.sqlite".MapAbsolutePath();
         public static string SqlServerDb = "~/App_Data/Database1.mdf".MapAbsolutePath();
-        public static string SqlServerBuildDb = "Data Source=localhost,48501\\SQLExpress;Network Library=DBMSSOCN;Initial Catalog=master;User Id=sa;Password=Test!tesT;Connect Timeout=120;MultipleActiveResultSets=True;";
+        public static string SqlServerBuildDb = Environment.GetEnvironmentVariable("MSSQL_CONNECTION") ?? "Data Source=localhost,48501\\SQLExpress;Network Library=DBMSSOCN;Initial Catalog=master;User Id=sa;Password=Test!tesT;Connect Timeout=120;MultipleActiveResultSets=True;";
         //public static string SqlServerBuildDb = "Data Source=localhost;Initial Catalog=TestDb;Integrated Security=SSPI;Connect Timeout=120;MultipleActiveResultSets=True";
 
-        public static string OracleDb = "Data Source=localhost:48401/XE;User ID=system;Password=test";
-        public static string MySqlDb_5_5 = "Server=localhost;Port=48201;Database=test;UID=root;Password=test;SslMode=none";
+        public static string OracleDb = Environment.GetEnvironmentVariable("ORACLE_CONNECTION") ?? "Data Source=localhost:48401/XE;User ID=system;Password=test";
+        public static string MySqlDb_5_5 = Environment.GetEnvironmentVariable("MYSQL_CONNECTION") ?? "Server=localhost;Port=48201;Database=test;UID=root;Password=test;SslMode=none";
         public static string MySqlDb_10_1 = "Server=localhost;Port=48202;Database=test;UID=root;Password=test;SslMode=none";
         public static string MySqlDb_10_2 = "Server=localhost;Port=48203;Database=test;UID=root;Password=test;SslMode=none";
         public static string MySqlDb_10_3 = "Server=localhost;Port=48204;Database=test;UID=root;Password=test;SslMode=none";
         public static string MySqlDb_10_4 = "Server=localhost;Port=48205;Database=test;UID=root;Password=test;SslMode=none";
-        public static string PostgresDb_9 = "Server=localhost;Port=48301;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200";
+        public static string PostgresDb_9 = Environment.GetEnvironmentVariable("PGSQL_CONNECTION") ?? "Server=localhost;Port=48301;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200";
         public static string PostgresDb_10 = "Server=localhost;Port=48302;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200";
         public static string PostgresDb_11 = "Server=localhost;Port=48303;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200";
-        public static string FirebirdDb_3 = @"User=SYSDBA;Password=masterkey;Database=/firebird/data/test.gdb;DataSource=localhost;Port=48101;Dialect=3;charset=ISO8859_1;MinPoolSize=0;MaxPoolSize=100;";
+        public static string FirebirdDb_3 = Environment.GetEnvironmentVariable("FIREBIRD_CONNECTION") ?? @"User=SYSDBA;Password=masterkey;Database=/firebird/data/test.gdb;DataSource=localhost;Port=48101;Dialect=3;charset=ISO8859_1;MinPoolSize=0;MaxPoolSize=100;";
 
 
         public static IOrmLiteDialectProvider DefaultProvider = SqlServerDialect.Provider;
@@ -134,6 +134,7 @@ namespace ServiceStack.OrmLite.Tests
 
             //OrmLiteConfig.DeoptimizeReader = true;
             LogManager.LogFactory = new ConsoleLogFactory(debugEnabled: Config.EnableDebugLogging);
+            
             switch (Dialect)
             {
                 case Dialect.Sqlite:
