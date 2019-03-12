cb5afb6d51cf9804e6da3a502e84bd826d55c81a
Scott Mackay <wwwlicious@users.noreply.github.com>
2019-03-12 09:07:28 +0000

    fix sql server test

--- src/ServiceStack.OrmLite.SqlServerTests/Expressions/AuthorUseCase.cs
+++ src/ServiceStack.OrmLite.SqlServerTests/Expressions/AuthorUseCase.cs
@@ -318,16 +318,6 @@ namespace ServiceStack.OrmLite.SqlServerTests.Expressions
                 // Tests for predicate overloads that make use of the expression visitor
                 author = db.Single<Author>(q => q.Name == "Jorge Garzon");
 
-                try
-                {
-                    author = db.Single<Author>(q => q.Name == "Does not exist");
-                    Assert.Fail();
-                }
-                catch
-                {
-                    //"Expected exception thrown, OK? True"
-                }
-
                 author = db.Single<Author>(q => q.Name == "Does not exist");
                 Assert.IsNull(author);
 
