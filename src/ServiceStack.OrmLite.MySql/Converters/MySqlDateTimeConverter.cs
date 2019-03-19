3a0ac75a268cbb5f29561f2d37d34a2699a744ba
Scott Mackay <wwwlicious@users.noreply.github.com>
2019-03-19 17:52:15 +0000

    adding mysql55 dialect

--- src/ServiceStack.OrmLite.MySql/Converters/MySqlDateTimeConverter.cs
+++ src/ServiceStack.OrmLite.MySql/Converters/MySqlDateTimeConverter.cs
@@ -8,12 +8,20 @@ namespace ServiceStack.OrmLite.MySql.Converters
     {
         public override object FromDbValue(object value)
         {
-            if (value is MySqlDateTime)
+            // TODO throws error if connectionstring option not set - https://stackoverflow.com/questions/5754822/unable-to-convert-mysql-date-time-value-to-system-datetime
+            if (value is MySqlDateTime time)
             {
-                var time = (MySqlDateTime)value;
                 return time.GetDateTime();
             }
             return base.FromDbValue(value);
         }
+    }
+
+    public class MySql55DateTimeConverter : MySqlDateTimeConverter
+    {
+        /// <summary>
+        /// CURRENT_TIMESTAMP as a default for DATETIME type is only available in 10.x. If you're using 5.5, it should a TIMESTAMP column
+        /// </summary>
+        public override string ColumnDefinition => "TIMESTAMP";
     }
 }
