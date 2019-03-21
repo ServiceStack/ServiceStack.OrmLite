08b1f0bc34fa686014ce4991dc9794491ce8841a
Scott Mackay <wwwlicious@users.noreply.github.com>
2019-03-21 00:31:19 +0000

    clean up TestConfig

--- tests/ServiceStack.OrmLite.Tests/TestConfig.cs
+++ tests/ServiceStack.OrmLite.Tests/TestConfig.cs
@@ -18,17 +18,19 @@ namespace ServiceStack.OrmLite.Tests
         public static string SqliteMemoryDb = EnvironmentVariable("SQLITE_CONNECTION", ":memory:");
         public static string SqlServerBuildDb = EnvironmentVariable("MSSQL_CONNECTION", "Data Source=tcp:localhost,48501\\SQLExpress;Initial Catalog=master;User Id=sa;Password=Test!tesT;Connect Timeout=120;MultipleActiveResultSets=True;");
         public static string OracleDb = EnvironmentVariable("ORACLE_CONNECTION", "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=48401))(CONNECT_DATA=(SID=XE)));User Id=system;Password=test;");
-        public static string MySqlDb_5_5 = EnvironmentVariable("MYSQL_CONNECTION", "Server=localhost;Port=48201;Database=test;UID=root;Password=test;SslMode=none");
+        
+        // TODO should dates be handled by reader instead of having to set config options?
+        public static string MySqlDb_5_5 = EnvironmentVariable("MYSQL_CONNECTION", "Server=localhost;Port=48201;Database=test;UID=root;Password=test;SslMode=none;Convert Zero Datetime=True;");
         public static string MySqlDb_10_1 = EnvironmentVariable("MYSQL101_CONNECTION", "Server=localhost;Port=48202;Database=test;UID=root;Password=test;SslMode=none");
         public static string MySqlDb_10_2 = EnvironmentVariable("MYSQL102_CONNECTION", "Server=localhost;Port=48203;Database=test;UID=root;Password=test;SslMode=none");
         public static string MySqlDb_10_3 = EnvironmentVariable("MYSQL103_CONNECTION", "Server=localhost;Port=48204;Database=test;UID=root;Password=test;SslMode=none");
         public static string MySqlDb_10_4 = EnvironmentVariable("MYSQL104_CONNECTION", "Server=localhost;Port=48205;Database=test;UID=root;Password=test;SslMode=none");
+        
         public static string PostgresDb_9 = EnvironmentVariable("PGSQL_CONNECTION", "Server=localhost;Port=48301;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200");
         public static string PostgresDb_10 = EnvironmentVariable("PGSQL10_CONNECTION", "Server=localhost;Port=48302;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200");
         public static string PostgresDb_11 = EnvironmentVariable("PGSQL10_CONNECTION", "Server=localhost;Port=48303;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200");
         public static string FirebirdDb_3 = EnvironmentVariable("FIREBIRD_CONNECTION", @"User=SYSDBA;Password=masterkey;Database=/firebird/data/test.gdb;DataSource=localhost;Port=48101;Dialect=3;charset=ISO8859_1;MinPoolSize=0;MaxPoolSize=100;");
         public static string VistaDb = EnvironmentVariable("VISTADB_CONNECTION", @"Data Source='|DataDirectory|\Database.vdb5'");
-        public static string SqliteFileDir = "~/App_Data/".MapAbsolutePath();
         public static string SqliteFileDb = "~/App_Data/db.sqlite".MapAbsolutePath();
         
         // The default provider and connection the DbFactory is initialised with
