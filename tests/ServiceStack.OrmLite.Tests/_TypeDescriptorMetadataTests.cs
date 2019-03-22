d5ee253ae83e2a33a8e2cc1444d537588b100e61
Scott Mackay <Scott Mackay wwwlicious@users.noreply.github.com>
2019-03-22 14:56:12 +0000

    fix _typedescrtiponmetadata test dialect checks

--- tests/ServiceStack.OrmLite.Tests/_TypeDescriptorMetadataTests.cs
+++ tests/ServiceStack.OrmLite.Tests/_TypeDescriptorMetadataTests.cs
@@ -14,39 +14,28 @@ namespace ServiceStack.OrmLite.Tests
         {
         }
 
-        private IDbConnection db;
-
-        [SetUp]
-        public void TestFixtureSetUp()
-        {
-            db = OpenDbConnection();
-        }
-
-        [TearDown]
-        public void TestFixtureTearDown()
-        {
-            db.Dispose();
-        }
-
         [Test]
         public void Can_add_AutoIncrement_Id_at_runtime()
         {
-            var model = new PersonDescriptor { FirstName = "Jimi", LastName = "Hendrix", Age = 27 };
+            using (var db = OpenDbConnection())
+            {
+                var model = new PersonDescriptor {FirstName = "Jimi", LastName = "Hendrix", Age = 27};
 
-            typeof(PersonDescriptor).GetProperty("Id")
-                .AddAttributes(new AutoIncrementAttribute());
+                typeof(PersonDescriptor).GetProperty("Id")
+                    .AddAttributes(new AutoIncrementAttribute());
 
-            db.DropAndCreateTable<PersonDescriptor>();
+                db.DropAndCreateTable<PersonDescriptor>();
 
-            var oldRows = db.Select<PersonDescriptor>();
+                var oldRows = db.Select<PersonDescriptor>();
 
-            db.Insert(model);
-            db.Insert(model);
-            model.Id = 0; // Oracle provider currently updates the id field so force it back to get an insert operation
-            db.Save(model);
+                db.Insert(model);
+                db.Insert(model);
+                model.Id = 0; // Oracle provider currently updates the id field so force it back to get an insert operation
+                db.Save(model);
 
-            var allRows = db.Select<PersonDescriptor>();
-            Assert.That(allRows.Count - oldRows.Count, Is.EqualTo(3));
+                var allRows = db.Select<PersonDescriptor>();
+                Assert.That(allRows.Count - oldRows.Count, Is.EqualTo(3));
+            }
         }
 
         [Test]
@@ -54,102 +43,103 @@ namespace ServiceStack.OrmLite.Tests
         [IgnoreProvider(Dialect.AnyPostgreSql, "Uses 'text' for strings by default")]
         public void Can_change_column_definition()
         {
-            typeof(DynamicCacheEntry)
-                .GetProperty("Data")
-                .AddAttributes(new StringLengthAttribute(7000));
+            using (var db = OpenDbConnection())
+            {
+                typeof(DynamicCacheEntry)
+                    .GetProperty("Data")
+                    .AddAttributes(new StringLengthAttribute(7000));
 
-            db.DropAndCreateTable<DynamicCacheEntry>();
+                db.DropAndCreateTable<DynamicCacheEntry>();
 
-            Assert.That(db.GetLastSql().NormalizeSql(), 
-                Does.Contain("Data VARCHAR(7000)".NormalizeSql()));
-            db.GetLastSql().Print();
+                Assert.That(db.GetLastSql().NormalizeSql(),
+                    Does.Contain("Data VARCHAR(7000)".NormalizeSql()));
+                db.GetLastSql().Print();
+            }
         }
 
         [Test]
         public void Can_Create_Table_with_MaxText_column()
         {
-            try
+            using (var db = OpenDbConnection())
             {
                 db.DropAndCreateTable<CacheEntry>();
-            }
-            catch (Exception)
-            {
-                db.DropAndCreateTable<CacheEntry>();
-            }
 
-            var sql = db.GetLastSql();
-            sql.Print();
+                var sql = db.GetLastSql();
+                sql.Print();
 
-            if (Dialect == Dialect.Sqlite)
-            {
-                Assert.That(sql, Does.Contain(" VARCHAR(1000000)"));
-            }
-            else if (Dialect == Dialect.AnyPostgreSql)
-            {
-                Assert.That(sql, Does.Contain(" TEXT"));
-            }
-            else if (Dialect == Dialect.AnyMySql)
-            {
-                Assert.That(sql, Does.Contain(" LONGTEXT"));
-            }
-            else if (Dialect == Dialect.AnyOracle)
-            {
-                Assert.That(sql, Does.Contain(" VARCHAR2(4000)"));
-            }
-            else if (Dialect == Dialect.SqlServer)
-            {
-                Assert.That(sql, Does.Contain(" VARCHAR(MAX)"));
+                if (Dialect == Dialect.Sqlite)
+                {
+                    Assert.That(sql, Does.Contain(" VARCHAR(1000000)"));
+                }
+                else if (Dialect == Dialect.AnyPostgreSql)
+                {
+                    Assert.That(sql, Does.Contain(" TEXT"));
+                }
+                else if (Dialect == Dialect.AnyMySql)
+                {
+                    Assert.That(sql, Does.Contain(" LONGTEXT"));
+                }
+                else if (Dialect == Dialect.AnyOracle)
+                {
+                    Assert.That(sql, Does.Contain(" VARCHAR2(4000)"));
+                }
+                else if (Dialect == Dialect.SqlServer)
+                {
+                    Assert.That(sql, Does.Contain(" VARCHAR(MAX)"));
+                }
             }
         }
 
         [Test]
         public void Can_Create_Table_with_MaxText_column_Unicode()
         {
-            var stringConverter = DialectProvider.GetStringConverter();
-            var hold = stringConverter.UseUnicode;
-            stringConverter.UseUnicode = true;
+            using (var db = OpenDbConnection())
+            {
+                var stringConverter = DialectProvider.GetStringConverter();
+                var hold = stringConverter.UseUnicode;
+                stringConverter.UseUnicode = true;
 
-            try
-            {
-                db.DropAndCreateTable<CacheEntry>();
-            }
-            catch (Exception)
-            {
-                db.DropAndCreateTable<CacheEntry>();
-            }
-            finally
-            {
-                stringConverter.UseUnicode = hold;
-            }
+                try
+                {
+                    db.DropAndCreateTable<CacheEntry>();
+                }
+                catch (Exception)
+                {
+                    db.DropAndCreateTable<CacheEntry>();
+                }
+                finally
+                {
+                    stringConverter.UseUnicode = hold;
+                }
 
-            var sql = db.GetLastSql();
-            sql.Print();
-            
-            if (Dialect.HasFlag(Dialect.Sqlite))
-            {
-                Assert.That(sql, Does.Contain(" NVARCHAR(1000000)"));
+                var sql = db.GetLastSql();
+                sql.Print();
+
+                if (Dialect.Sqlite.HasFlag(Dialect))
+                {
+                    Assert.That(sql, Does.Contain(" NVARCHAR(1000000)"));
+                }
+                else if (Dialect.AnyPostgreSql.HasFlag(Dialect))
+                {
+                    Assert.That(sql, Does.Contain(" TEXT"));
+                }
+                else if (Dialect.AnyMySql.HasFlag(Dialect))
+                {
+                    Assert.That(sql, Does.Contain(" LONGTEXT"));
+                }
+                else if (Dialect.AnyOracle.HasFlag(Dialect))
+                {
+                    Assert.That(sql, Does.Contain(" NVARCHAR2(4000)"));
+                }
+                else if (Dialect.Firebird.HasFlag(Dialect))
+                {
+                    Assert.That(sql, Does.Contain(" VARCHAR(10000)"));
+                }
+                else
+                {
+                    Assert.That(sql, Does.Contain(" NVARCHAR(MAX)"));
+                }
             }
-            else if (Dialect.HasFlag( Dialect.AnyPostgreSql))
-            {
-                Assert.That(sql, Does.Contain(" TEXT"));
-            }
-            else if (Dialect.HasFlag(Dialect.AnyMySql))
-            {
-                Assert.That(sql, Does.Contain(" LONGTEXT"));
-            }
-            else if (Dialect.HasFlag( Dialect.AnyOracle))
-            {
-                Assert.That(sql, Does.Contain(" NVARCHAR2(4000)"));
-            }
-            else if (Dialect.HasFlag(Dialect.Firebird))
-            {
-                Assert.That(sql, Does.Contain(" VARCHAR(10000)"));
-            }
-            else
-            {
-                Assert.That(sql, Does.Contain(" NVARCHAR(MAX)"));
-            }
-            
         }
     }
 
