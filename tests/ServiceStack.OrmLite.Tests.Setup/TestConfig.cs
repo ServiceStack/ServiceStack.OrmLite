fdb2c73b1584a6cdf30b500b83ee33ee6bbb8c64
Demis Bellot <demis.bellot@gmail.com>
2019-04-07 18:04:12 -0400

    Update TestConfig.cs

--- tests/ServiceStack.OrmLite.Tests.Setup/TestConfig.cs
+++ tests/ServiceStack.OrmLite.Tests.Setup/TestConfig.cs
@@ -290,6 +290,8 @@ namespace ServiceStack.OrmLite.Tests
                 {
                     db.ExecuteSql(pgExtension);
                 }
+
+                var dialectProvider = db.GetDialectProvider();
                 var schemaName = dialectProvider.NamingStrategy.GetSchemaName("Schema");
                 db.ExecuteSql($"CREATE SCHEMA IF NOT EXISTS {dialectProvider.GetQuotedName(schemaName)}");
             }
