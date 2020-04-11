f237d83915ce9e7482c8661c35766206e16df5ed
Demis Bellot <demis.bellot@gmail.com>
2020-04-11 15:40:52 +0800

    Add helpers for PgSql.Array

--- src/ServiceStack.OrmLite.PostgreSQL/PgSql.cs
+++ src/ServiceStack.OrmLite.PostgreSQL/PgSql.cs
@@ -8,5 +8,14 @@ namespace ServiceStack.OrmLite.PostgreSQL
             new NpgsqlParameter(name, PostgreSqlDialect.Instance.GetDbType<T>()) {
                 Value = value
             };
+
+        public static string Array<T>(params T[] items) =>
+            "ARRAY[" + PostgreSqlDialect.Provider.SqlSpread(items) + "]";
+
+        public static string Array<T>(T[] items, bool nullIfEmpty) => nullIfEmpty
+            ? "ARRAY[" + NullIfEmpty(PostgreSqlDialect.Provider.SqlSpread(items)) + "]"
+            : "ARRAY[" + PostgreSqlDialect.Provider.SqlSpread(items) + "]";
+
+        public static string NullIfEmpty(string sql) => sql == "ARRAY[]" ? "null" : sql;
     }
 }
--- tests/ServiceStack.OrmLite.PostgreSQL.Tests/PgSqlTests.cs
+++ tests/ServiceStack.OrmLite.PostgreSQL.Tests/PgSqlTests.cs
@@ -14,5 +14,19 @@ namespace ServiceStack.OrmLite.PostgreSQL.Tests
             Assert.That(PgSql.Param("p", new [] { 1 }).NpgsqlDbType, 
                 Is.EqualTo(NpgsqlDbType.Integer | NpgsqlDbType.Array));
         }
+
+        [Test]
+        public void Does_PgSqlArray()
+        {
+            Assert.That(PgSql.Array((string[])null), Is.EqualTo("ARRAY[]"));
+            Assert.That(PgSql.Array(new string[0]), Is.EqualTo("ARRAY[]"));
+            Assert.That(PgSql.Array(new int[0]), Is.EqualTo("ARRAY[]"));
+            Assert.That(PgSql.Array(1,2,3), Is.EqualTo("ARRAY[1,2,3]"));
+            Assert.That(PgSql.Array("A","B","C"), Is.EqualTo("ARRAY['A','B','C']"));
+            Assert.That(PgSql.Array("A'B","C\"D"), Is.EqualTo("ARRAY['A''B','C\"D']"));
+            
+            Assert.That(PgSql.Array(new string[0], nullIfEmpty:true), Is.EqualTo("null"));
+            Assert.That(PgSql.Array(new[]{"A","B","C"}, nullIfEmpty:true), Is.EqualTo("ARRAY['A','B','C']"));
+        }
     }
 }
