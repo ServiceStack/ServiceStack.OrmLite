7d235a8a229a3a8a57b01a0a8b741af07870b050
Demis Bellot <demis.bellot@gmail.com>
2021-01-21 01:31:55 +0800

    Wire SQLite.Cil

--- src/ServiceStack.OrmLite.Sqlite.Cil/SqliteOrmLiteDialectProvider.cs
+++ src/ServiceStack.OrmLite.Sqlite.Cil/SqliteOrmLiteDialectProvider.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Data;
-using SQLitePCL;
+using Microsoft.Data.Sqlite;
+using ServiceStack.OrmLite.Sqlite.Converters;
 
 namespace ServiceStack.OrmLite.Sqlite
 {
-    //TODO: Find ADO.NET Wrapper for SQLitePCL 
     public class SqliteOrmLiteDialectProvider : SqliteOrmLiteDialectProviderBase
     {
         public static SqliteOrmLiteDialectProvider Instance = new();
 
+        public SqliteOrmLiteDialectProvider()
+        {
+            base.RegisterConverter<DateTime>(new SqliteDataDateTimeConverter());
+            base.RegisterConverter<Guid>(new SqliteDataGuidConverter());
+        }
+
         protected override IDbConnection CreateConnection(string connectionString)
         {
-            // return new SqliteConnection(connectionString);
-            throw new NotImplementedException();
+            // Microsoft.Data.Sqlite no like
+            connectionString = connectionString
+                .Replace(";Version=3", "")
+                .Replace(";New=True", "")
+                .Replace(";Compress=True", ""); 
+            return new SqliteConnection(connectionString);
         }
 
         public override IDbDataParameter CreateParam()
         {
-            // return new SqliteParameter();
-            throw new NotImplementedException();
+            return new SqliteParameter();
         }
     }
 }
