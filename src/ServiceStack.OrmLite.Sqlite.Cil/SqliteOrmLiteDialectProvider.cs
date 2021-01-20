93fb52e3a518234cdac3c038416e9bf107c1cd8d
Demis Bellot <demis.bellot@gmail.com>
2021-01-20 16:30:35 +0800

    Add holding project for SQLitePCLRaw.bundle_cil

---
+++ src/ServiceStack.OrmLite.Sqlite.Cil/SqliteOrmLiteDialectProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using SQLitePCL;
+
+namespace ServiceStack.OrmLite.Sqlite
+{
+    //TODO: Find ADO.NET Wrapper for SQLitePCL 
+    public class SqliteOrmLiteDialectProvider : SqliteOrmLiteDialectProviderBase
+    {
+        public static SqliteOrmLiteDialectProvider Instance = new();
+
+        protected override IDbConnection CreateConnection(string connectionString)
+        {
+            // return new SqliteConnection(connectionString);
+            throw new NotImplementedException();
+        }
+
+        public override IDbDataParameter CreateParam()
+        {
+            // return new SqliteParameter();
+            throw new NotImplementedException();
+        }
+    }
+}
