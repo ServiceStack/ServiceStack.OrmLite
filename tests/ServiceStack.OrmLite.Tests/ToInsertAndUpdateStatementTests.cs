758a96428db32d4573c422756ff6719a1b0f6af6
Darren Reid <darren@reidmail.org>
2021-02-24 14:19:58 +1100

    To insert statement dbnull check (#652)
    * Fix for when table class has reference type (treated as TEXT or JSON column) and is provided null. Previously generated `{}` for null values with if inserted and then retrieved results in different instance values.
    
    * Add additional test for ToInsertAndUpdateStatementTests.cs for ref with populated values.
    
    * Add additional test for ToInsertAndUpdateStatementTests.cs using `DateTime?` .

--- tests/ServiceStack.OrmLite.Tests/ToInsertAndUpdateStatementTests.cs
+++ tests/ServiceStack.OrmLite.Tests/ToInsertAndUpdateStatementTests.cs
@@ -45,5 +45,56 @@ namespace ServiceStack.OrmLite.Tests
                 Assert.That(insertedRow.Equals(row));
             }
         }
+
+        [Test]
+        public void Correct_DbNull_in_ToInsertStatement()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<PersonWithReferenceType>();
+                var row = PersonWithReferenceType.TestValues[0];
+
+                var sql = db.ToInsertStatement(row);
+                sql.Print();
+                db.ExecuteSql(sql);
+
+                var insertedRow = db.SingleById<PersonWithReferenceType>(row.Id);
+                Assert.That(insertedRow.Equals(row));
+            }
+        }
+        
+        [Test]
+        public void Correct_Ref_in_ToInsertStatement()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<PersonWithReferenceType>();
+                var row = PersonWithReferenceType.TestValues[1];
+
+                var sql = db.ToInsertStatement(row);
+                sql.Print();
+                db.ExecuteSql(sql);
+
+                var insertedRow = db.SingleById<PersonWithReferenceType>(row.Id);
+                Assert.That(insertedRow.Equals(row));
+            }
+        }
+        
+        [Test]
+        public void Correct_nullable_in_ToInsertStatement()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<TestProduct>();
+                var row = TestProduct.TestValues[0];
+
+                var sql = db.ToInsertStatement(row);
+                sql.Print();
+                db.ExecuteSql(sql);
+
+                var insertedRow = db.SingleById<TestProduct>(row.Id);
+                Assert.AreEqual(insertedRow, row);
+            }
+        }
     }
 }
