1bc4ad07fd9fa8cffb45952dbf30099288e08d09
Demis Bellot <demis.bellot@gmail.com>
2022-01-24 22:08:34 +0800

    Remove legacy VistaDB support

--- src/ServiceStack.OrmLite.VistaDB/Converters/VistaDbStringConverters.cs
+++
@@ -1,9 +0,0 @@
-﻿using ServiceStack.OrmLite.Converters;
-
-namespace ServiceStack.OrmLite.VistaDB.Converters
-{
-    public class VistaDbStringConverter : StringConverter
-    {
-        public override string MaxColumnDefinition => UseUnicode ? "NVARCHAR(MAX)" : "VARCHAR(MAX)";
-    }
-}
